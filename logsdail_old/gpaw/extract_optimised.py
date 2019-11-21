936fe1cebe1899ce2f144b72b2e552158f8494af
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2019-11-21 16:10:42 +0000

    Removed all outdated scripts and rearranged

--- logsdail_old/gpaw/extract_optimised.py
+++
@@ -1,9 +0,0 @@
-from ase.io import write, PickleTrajectory
-import os
-
-#Read atoms
-for file in os.listdir("."):
-    if file.endswith(".traj"):
-        traj=PickleTrajectory(file)
-        atoms=traj[-1]
-	write(file[:-5]+"_opt.xyz",atoms)
