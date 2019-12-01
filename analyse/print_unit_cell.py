92053570ee1e0cf220d67e3af67aa747d356ceee
Andy Logsdail <LogsdailA@cardiff.ac.uk>
2019-12-01 17:17:07 +0000

    Tidied the unit cell analysis

--- analyse/print_unit_cell.py
+++ analyse/print_unit_cell.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
 
-import ase.io.vasp
+from ase.io import read
 import sys
-from numpy import pi
 
-max_distance = 3
+if len(sys.argv) < 2):
+    print("Runtime arguments needed: ./script filename")
+    exit()
 
-cell = ase.io.read(sys.argv[1])
+for filename in sys.argv[1:]:
+    cell = read(filename)
+    print(filename, cell.get_cell_lengths_and_angles())
 
-print(cell.get_cell_lengths_and_angles())
-
-for i in range(len(cell)):
-    if cell.numbers[i] != 8:
-        for j in range(len(cell)):
-            distance = cell.get_distance(i,j)
-            if cell.numbers[i] != cell.numbers[j] and distance < max_distance:
-                for k in range(len(cell)):
-                    distance = cell.get_distance(j,k)
-                    if cell.numbers[i] == cell.numbers[k] and distance < max_distance:
-                        angle = cell.get_angle([i,j,k])*180. / pi
-                        if angle > 80 and angle < 100:
-                            print(i, j, k, cell.numbers[i], cell.numbers[j], cell.numbers[k], distance, angle)
