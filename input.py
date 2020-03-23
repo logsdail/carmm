8c980317df240f5c34b4baebce94e1a7ca100e2b
JackWarren23 <62541339+JackWarren23@users.noreply.github.com>
2020-03-23 10:50:20 +0000

    Add files via upload

--- input.py
+++ input.py
@@ -2,24 +2,25 @@ from ase import Atoms
 from ase.calculators.aims import Aims, AimsCube
 from ase.optimize import QuasiNewton
 import os
-
+from ase.io import read
+import numpy as np
+from ase.io.read_vasp import (filename=POSCAR)
 os.environ['ASE_AIMS_COMMAND']="mpirun -np "+os.environ['SLURM_NTASKS']+" /home/scw1057/software/fhi-aims/bin/aims."+os.environ['VERSION']+".scalapack.mpi.x"
 os.environ['AIMS_SPECIES_DIR']="/home/scw1057/software/fhi-aims/species_defaults/light"   # Light settings
 #os.environ['AIMS_SPECIES_DIR']="/home/c.sacal6/software/fhi-aims-species-defaults/tight" # Tight settings
 
-atom = Atoms('N', calculator=Aims(xc='pbe'))
-e_atom = atom.get_potential_energy()
-
-d=1.1
-molecule = Atoms('2N', [(0., 0., 0.), (0., 0., d)])
-molecule.set_calculator(Aims(xc='pbe'))
-e_molecule = molecule.get_potential_energy()
-
-e_atomization = e_molecule - 2* e_atom 
-
-print('Nitrogen atom energy: %5.2f eV' % e_atom)
-print('Nitrogen molecule energy: %5.2f eV' % e_molecule)
-print('Atomization energy: %5.2f eV' % -e_atomization)
+atoms = read('POSCAR')
 
 
+calc = Aims(xc='PBE',
+            output=['dipole'],
+            sc_accuracy_etot=1e-6,
+            sc_accuracy_eev=1e-3,
+            sc_accuracy_rho=1e-6,
+            sc_accuracy_forces=1e-4,
+            cubes=HBBC_cube)
 
+
+HNBBC.set_calculator(calc)
+dynamics = QuasiNewton(atoms, trajectory='HNBBC.traj')
+dynamics.run(fmax=0.01)
