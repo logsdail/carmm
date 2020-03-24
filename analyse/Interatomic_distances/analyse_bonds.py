30b8f9c9897d8a6a83be8900dcdf212c5b1861bd
ikowalec <aikey2011@gmail.com>
2020-03-24 15:12:52 +0000

    In this state these should be useful to anyone for quick FULL analysis.

--- analyse/Interatomic_distances/analyse_bonds.py
+++ analyse/Interatomic_distances/analyse_bonds.py
@@ -5,22 +5,76 @@ from ase.io import read
 from ase.geometry.analysis import Analysis
 from ase import Atoms
 
+## Use analyse_all_bonds and analyse_all_angles unless you are interested
+## in specific values that are hard to get using visualizer.
+## The functions take either a filename or a Atoms object.
+
+
+def analyse_all_bonds(model):
+    from itertools import combinations_with_replacement #combination as AB = BA for bonds, avoiding redundancy
+    if isinstance(model, str) is True: #read file or Atoms object
+        model = read(model)
+
+    analysis = Analysis(model)
+    dash = "-"*40
+    list_of_symbols = list(set(model.get_chemical_symbols())) #set to ensure unique chemical symbols list
+    all_bonds = combinations_with_replacement(list_of_symbols, 2)
+
+    print(dash) #Table heading
+    print('{:<6.5s}{:<6.5s}{:>4.10s}{:^13.10s}{:>4.10s}'.format("Bond","Count","Average", "Minimum", "Maximum"))
+    print(dash)
+
+    for bonds in all_bonds:     #iterate over all arrangements of chemical symbols
+        A = bonds[0]
+        B = bonds[1]
+
+        print_AB = A+'-'+B
+        AB_Bonds = analysis.get_bonds(A,B)
+
+        if not AB_Bonds == [[]]:   #make sure bond exist before retrieving values
+            AB_BondsValues = analysis.get_values(AB_Bonds)
+            print('{:<6.5s}{:<6.0f}{:>4.6f}{:^12.6f}{:>4.6f}'.format(
+            print_AB,len(AB_BondsValues[0]),np.average(AB_BondsValues),np.amin(AB_BondsValues),np.amax(AB_BondsValues)))
+
+def analyse_all_angles(model):
+    from itertools import product #product to get all possible arrangements
+    if isinstance(model, str) is True: #read file or Atoms object
+        model = read(model)
+
+    analysis = Analysis(model)
+    dash = "-"*40
+    list_of_symbols = list(set(model.get_chemical_symbols())) #set to ensure unique chemical symbols list
+    all_angles = product(list_of_symbols, repeat=3)
+
+    print(dash)     #Table heading
+    print('{:<6.5s}{:<6.5s}{:>4.10s}{:^13.10s}{:>4.10s}'.format("Angle","Count","Average", "Minimum", "Maximum"))
+    print(dash)
+
+    for angles in all_angles:     #iterate over all arrangements of chemical symbols
+        A = angles[0]
+        B = angles[1]
+        C = angles[2]
+
+        print_ABC = A+'-'+B+'-'+C
+        ABC_Angle = analysis.get_angles(A,B,C)
+
+        if not ABC_Angle == [[]]:   #make sure angles exist before retrieving values
+            ABC_AngleValues = analysis.get_values(ABC_Angle)
+            print('{:<6.5s}{:<6.0f}{:>4.4f}{:^12.4f}{:>4.4f}'.format(
+            print_ABC,len(ABC_Angle[0]), np.average(ABC_AngleValues),np.amin(ABC_AngleValues),np.amax(ABC_AngleValues)))
+
+
 # check A-B distance and A-B-C angles
 # A,B,C are chemical symbols
-def analyse_bonds(file, A, B, C):
-    file = read(file)
-    analysis = Analysis(file)
+def analyse_bonds(model, A, B):
+    if isinstance(model, str) is True: #read file or Atoms object
+        model = read(model)
+    analysis = Analysis(model)
+    dash = "-"*40
 
     print_AB = A +"-"+B
-    print_ABC = A +"-"+B+"-"+C
-
     AB_Bonds = analysis.get_bonds(A,B)
-    ABC_Angle = analysis.get_angles(A,B,C)
-
     AB_BondsValues = analysis.get_values(AB_Bonds)
-    ABC_AngleValues = analysis.get_values(ABC_Angle)
-
-    dash = "-"*40
 
     print(dash)
     print(print_AB+"       Distance / Angstrom")
@@ -28,6 +82,16 @@ def analyse_bonds(file, A, B, C):
     print('{:<6.5s}{:>4.10s}{:^13.10s}{:>4.10s}'.format("count","average", "minimum", "maximum"))
     print('{:<6.0f}{:>4.6f}{:^12.6f}{:>4.6f}'.format(len(AB_BondsValues[0]),np.average(AB_BondsValues),np.amin(AB_BondsValues),np.amax(AB_BondsValues)))
 
+def analyse_angles(model, A, B, C):
+    if isinstance(model, str) is True: #read file or Atoms object
+        model = read(model)
+    analysis = Analysis(model)
+    dash = "-"*40
+
+    print_ABC = A +"-"+B+"-"+C
+
+    ABC_Angle = analysis.get_angles(A,B,C)
+    ABC_AngleValues = analysis.get_values(ABC_Angle)
     print(dash)
     print(print_ABC+"       Angle / Degrees")
     print(dash)
