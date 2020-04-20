33e5c222dd29c708fd26553151a372fb1efc5a96
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2020-04-20 11:30:50 +0100

    Added assertion for the Wulff Construction tests

--- examples/build_wullf_construction.py
+++ examples/build_wullf_construction.py
@@ -10,16 +10,21 @@ TODO: Description Needed
 """
 
 from wulffpack import SingleCrystal
-from wulffpack import Decahedron
-from wulffpack import Icosahedron
 from ase.build import bulk
 from ase.io import write
 prim = bulk('Pd', crystalstructure='fcc', a=3.909)
 surface_energies = {(1, 1, 1): 1.1,
                     (1, 0, 1): 1.0,
                     (2, 1, 0): 1.0}
+
 particle = SingleCrystal(surface_energies,
                          primitive_structure=prim,
                          natoms=5000)
-particle.view()
-write('fcc_single_crystal.xyz', particle.atoms)
+
+#### Assertion test to make sure code works ####
+assert(abs(particle.area - 8885.065282 < 1e-5 ))
+assert(len(particle.atoms) == 5079)
+########
+
+#particle.view()
+#write('fcc_single_crystal.xyz', particle.atoms)
