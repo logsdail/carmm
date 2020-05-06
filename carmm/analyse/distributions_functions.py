1acf9b1f896c04d61ca8ff3543dc4700113eeba6
owainbeynon <54601295+owainbeynon@users.noreply.github.com>
2020-05-06 11:29:16 +0100

    Create a distribution function
    Outputs a plot giving a distribution of distances of all atoms from a selected atom

---
+++ carmm/analyse/distributions_functions.py
@@ -0,0 +1,62 @@
+
+def distance_distributon_function(model):
+    '''Returns a plot of the distribution of the distance between each atom from atom_0.
+    plot is currently a frequency vs distance. Current usage is for periodic solids
+    TODO: - normalise distribution, atoms at the opposite end of the cell are actually close together - need to account for this.
+          - currently plot is a histogram need to chnage to be a gaussian '''
+    from ase.io import read
+    from ase.geometry.analysis import Analysis
+    from matplotlib import pyplot as plt
+    import numpy as np
+    import seaborn as sns
+    import scipy.stats as sps
+    #Read file or Atoms object
+    if isinstance(model, str) is True:
+        model = read(model)
+
+     #Create a variable which represents the amount of atoms in the system
+    positions = model.get_positions()
+    A = len(positions)
+    #Create an array with the all the distances from atom 0
+    distances = model.get_distances(0, range(A), mic=False, vector=False)
+    #plot these values as a histogram
+    plt.figure()
+    plt.hist(distances ,bins=A, density=False, color='b', histtype='step', align='mid', stacked=True)
+    plt.xlabel('r/Å', fontsize=15)
+    plt.ylabel('n(r)', fontsize=15)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.title('Distribution Function', fontsize=15)
+    plt.show()
+
+
+def radial_distributon_function(model):
+    '''Returns a plot of the distribution of the distance between each atom from atom_0.
+    plot is currently a frequency vs distance. Current usage is for periodic solids
+     TODO: - Ammend script to account for a radial function
+            - Normalise distribution
+            - Gaussians over the histrogram '''
+    from ase.io import read
+    from ase.geometry.analysis import Analysis
+    from matplotlib import pyplot as plt
+    import numpy as np
+    import seaborn as sns
+    import scipy.stats as sps
+    #Read file or Atoms object
+    if isinstance(model, str) is True:
+        model = read(model)
+
+    # Create a variable which represents the amount of atoms in the system
+    positions = model.get_positions()
+    A = len(positions)
+    # Create an array with the all the distances from atom 0
+    distances = model.get_distances(0, range(A), mic=False, vector=False)
+    # plot these values as a histogram
+    plt.figure()
+    plt.hist(distances ,bins=A, density=False, color='b', histtype='step', align='mid', stacked=True)
+    plt.xlabel('r/Å', fontsize=15)
+    plt.ylabel('g(r)', fontsize=15)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.title('Radial Distribution Function', fontsize=15)
+    plt.show()
