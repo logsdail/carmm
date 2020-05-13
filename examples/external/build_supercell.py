80cb106209184be43c6090e2fdbe3ffbfdca90b3
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2020-05-13 13:53:45 +0100

    Picked up an error in the old external tutorials.

--- examples/external/build_supercell.py
+++ examples/external/build_supercell.py
@@ -5,11 +5,9 @@ Simple example to show functionality to create a supercell of any given model
 '''
 
 from ase.build import bulk
-from software.build.facets import create_supercell
 
 atoms = bulk('Pd', crystalstructure='fcc', a=3.909)
-supercell = create_supercell(atoms, 3, 3, 3)
+supercell = atoms*(3,3,3)
 
-#### Assertion test ####
-assert(len(supercell) == 27)
-#########
+from ase.visualize import view
+view(supercell)
