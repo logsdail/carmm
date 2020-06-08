9e541d532d9ef5f3d8b4cdfcae5293bc0d605c06
owainbeynon <54601295+owainbeynon@users.noreply.github.com>
2020-06-08 12:51:14 +0100

    Update analyse_distribution_functions.py

--- examples/analyse_distribution_functions.py
+++ examples/analyse_distribution_functions.py
@@ -6,12 +6,23 @@ TODO: Add assertion tests
 '''
 
 def test_analyse_distribution_function():
-    from carmm.analyse.distribution_functions import distance_distributon_function
+    from carmm.analyse.distribution_functions import distance_distribution_function
 
     #Build a model
     from data.model_gen import get_example_slab as slab
     slab = slab(adsorbate=True)
 
-    distance_distributon_function(slab)
+    distance_distribution_function(slab)
 
 test_analyse_distribution_function()
+
+def test_analyse_radial_distribution_function():
+    from carmm.analyse.distribution_functions import radial_distribution_function
+
+    #Build model
+    from data.model_gen import get_example_slab as slab_2
+    slab_2 = slab_2(adsorbate=True)
+
+    radial_distribution_function(slab_2, 10)
+
+test_analyse_radial_distribution_function()
