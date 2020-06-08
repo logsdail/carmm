120f791f2713ce3e92ba67bb49aa3db2e68563a2
Andy Logsdail <LogsdailA@cardiff.ac.uk>
2020-06-08 11:31:30 +0100

    Integrated hostname check directly into aims_calculator, so user doesn't need to see any difference when working on Hawk, Isambard etc. - the usage is the same

--- carmm/run/aims_calculator.py
+++ carmm/run/aims_calculator.py
@@ -31,7 +31,7 @@ def get_aims_calculator(dimensions, k_grid=None):
 
     return fhi_calc
 
-def get_aims_and_sockets_calculator(dimensions, k_grid=None, port=12345, host='localhost', logfile='socketio.log'):
+def get_aims_and_sockets_calculator(dimensions, k_grid=None, port=12345, host=None, logfile='socketio.log'):
     '''
     Method to return a sockets calculator (for i-Pi based socket connectivity)
     and also an associated FHI-aims calculator for ASE
@@ -45,13 +45,25 @@ def get_aims_and_sockets_calculator(dimensions, k_grid=None, port=12345, host='l
             The port for connection between FHI-aims and ASE with i-Pi sockets.
             This is fairly arbitrary as long as it doesn't clash with local settings.
         host: String
-            Name of host computer for ASE. Necessary for calculations where MPI runs on the compute nodes
+            Name of host computer for ASE. Necessary for calculations where MPI runs on the compute nodes.
+            This is now defaulted to None, and then will self-identify the host name if unidentified.
+            Long-term, we should consider removing this variable to insulate the users from having to set it.
+        logfile: String
+            Location of output from sockets communication.
 
     Returns:
         Socket_calc: Wrapper for ASE calculator
             Used for i-Pi connectvity, and should be assigned to optimisation/dynamics Object
         FHI_calc: FHI-aims ASE calculator
     '''
+
+    if host is None:
+        # On machines where ASE and FHI-aims are run separately (e.g. ASE on login node, FHI-aims on compute nodes)
+        # we need to specifically state what the name of the login node is so the two packages can communicate
+        # In order to manage this communication in all situations, here we will find and use the hostname *even*
+        # if on the same computer (it should work irrespective)
+        import socket
+        host=socket.gethostname()
 
     fhi_calc = get_aims_calculator(dimensions, k_grid)
     # Add in PIMD command to get sockets working
