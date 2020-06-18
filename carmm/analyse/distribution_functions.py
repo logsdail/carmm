763d1da2a582437f30d9b946bd9183521d14f656
owainbeynon <54601295+owainbeynon@users.noreply.github.com>
2020-06-18 23:02:14 +0100

    Update distribution_functions.py

--- carmm/analyse/distribution_functions.py
+++ carmm/analyse/distribution_functions.py
@@ -1,5 +1,5 @@
 def distance_distribution_function(model):
-    '''Returns a plot of the distribution of the distance between each atom from atom_0.
+    '''Returns a plot of the distribution of the distance between all atoms
     plot is currently a frequency vs distance. Current usage is for periodic solids
    TODO: - currently plot is a histogram need to change to be a gaussian - separate the bins?
            AJL: Could the plot be at the taste of the user, and the function just returns the data?
@@ -7,22 +7,20 @@ def distance_distribution_function(model):
          - Only calculates RDF with respect the first atom - needs to be generalised:
             - what about for any other atom of interest?
             - what about averaging over all atoms, as per standard EXAFS?
+              OB: amended distance_distribution to average over all atoms
+
         '''
     from matplotlib import pyplot as plt
-    #Read file or Atoms object
-    if isinstance(model, str) is True:
-        from ase.io import read
-        model = read(model)
+    # get all distances in the model
+    distances = model.get_all_distances(mic=True, vector=False)
+    # current distance variable is an array of coordinates for each atom, average position for each atom:
+    a = (sum(distances) / 3)
+    # get the relative positions of atoms to eachother
+    norm = a - min(a)
 
-
-    #Create a variable which represents the amount of atoms in the system
-    positions = model.get_positions()
-    A = len(positions)
-    #Create an array with the all the distances from atom 0
-    distances = model.get_distances(0, range(A), mic=True, vector=False)
-    #plot these values as a histogram
+    # plot these values as a histogram
     plt.figure()
-    plt.hist(distances ,bins=A, density=False, color='b', histtype='step', align='mid', stacked=True)
+    plt.hist(norm, bins=len(distances), density=False, histtype='step', align='mid', stacked=True)
     plt.xlabel('r/Å', fontsize=15)
     plt.ylabel('n(r)', fontsize=15)
     plt.xticks(fontsize=15)
@@ -31,7 +29,7 @@ def distance_distribution_function(model):
     plt.show()
 
 
-def radial_distribution_function(model, radius):
+def radial_distribution_function(model, radius, position):
     '''Returns a plot of the distribution of the distance between each atom from atom_0.
     plot is currently a frequency vs distance. Current usage is for periodic solids
     This script will create a radius around a given model and calculate the distance of this new model
@@ -57,8 +55,8 @@ def radial_distribution_function(model, radius):
     radial_model = cutout_sphere(super_cell, number_atoms_in_model, radius)
     positions_2 = radial_model.get_positions()
     number_atoms_in_radial_model = len(positions_2)
-    # Create an array with the all the distances from atom 0
-    distances = radial_model.get_distances(0, range(number_atoms_in_radial_model), mic=True, vector=False)
+    # Create an array with the all the distances from atom the variable 'position'
+    distances = radial_model.get_distances(position, range(number_atoms_in_radial_model), mic=True, vector=False)
 
     # plot these values as a histogram
     plt.figure()
