0f811757dbb8a43e0e74c145c285cae24ec37e9d
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2020-06-19 16:50:16 +0100

    Minor changes - moved file to build_dissociation and tidied/added some comments

--- examples/build_dissociation.py
+++ examples/build_dissociation.py
@@ -12,12 +12,12 @@ which can be optimised by the user - toy model of 2 Cu ad-atoms on Au surface.
 
 '''
 
-
 def test_dissociation():
     from carmm.build.neb.bond_length_scan import dissociation
-    from ase.build import add_adsorbate
     from examples.data.model_gen import get_example_slab
+    
     from ase import Atoms
+    from ase.build import add_adsorbate
     from ase.calculators.emt import EMT
     from ase.optimize import BFGS
 
@@ -28,17 +28,20 @@ def test_dissociation():
     opt = BFGS(slab)
     opt.run(fmax=0.05)
 
-    # move adsorbed atoms (indices 18,19) away from each other
+    # move adsorbed Cu atoms (indices 18,19) away from each other
     atoms_list, distance_list = dissociation(slab, 18, 19, step_size=0.2, n_steps=10)
 
-
+    # Do we need the optimisation for all species as part of the test? 
+    # No idea how long this takes - could be super quick!
     for atoms in atoms_list:
         atoms.set_calculator(EMT())
         opt = BFGS(atoms)
         opt.run(fmax=0.05)
 
-    from ase.visualize import view
-    view(atoms_list)
+    #TODO: Add in an assertion test so that functionality can actually be verified.
+    
+    #from ase.visualize import view
+    #view(atoms_list)
     #print(distance_list)
 
 test_dissociation()
