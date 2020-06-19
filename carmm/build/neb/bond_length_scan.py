a2bcd4b1b7d9cd05315267fe439824fd493c9e12
ikowalec <aikey2011@gmail.com>
2020-06-19 16:40:15 +0100

    Separated example, now returns Atoms objects with constraints (optimisation up to user) and list of fixed bond lengths

--- carmm/build/neb/bond_length_scan.py
+++ carmm/build/neb/bond_length_scan.py
@@ -4,12 +4,15 @@ Created on Fri 19/06/2020
 @author: Igor Kowalec, David Willock
 '''
 
-def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, traj_prefix="mep", fmax=0.05):
+def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20):
+
     '''
-    This function is a tool for investigating atom dissociation from a molecule.
+    This function is a tool for investigating bond dissociation.
     Bond length of interest is fixed and is increased by step_size in each iteration.
     This aims to help with obtaining activation energies for surface calculations, e.g. hydrogenation,
     where metastability of optimal starting position is often low and thus hard to obtain.
+    Returns a list of Atoms objects with changed positions and constraints applied,
+    which can be optimised by the user.
 
     Args:
         atoms: Atoms object
@@ -20,74 +23,45 @@ def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, traj_prefix="mep", f
         step_size: float
             Distance moved during dissociation in Angstrom per iteration
         n_steps: int
-            Number of steps to calculate
-        traj_prefix: str
-            Naming scheme that will be followed for all geometry optimisation .traj files
+            Total number of steps, including initial
         fmax: float
             Force convergence criterion for geometry optimisation, default is 0.01 eV/Angstrom
     '''
 
-    from ase.io.trajectory import Trajectory
-    from ase.optimize import BFGS
     from ase.constraints import FixBondLength
     import copy
-
-    # Helper function Atom-Atom distance
-    def atom_dist(atoms, i1, i2):
-        import numpy as np
-        pos_diff = atoms[i1].position - atoms[i2].position
-        distance = np.linalg.norm(pos_diff)
-
-        return distance
-
+    import numpy as np
 
     # retrieve initial atom - atom distance
-    initial_dist = atom_dist(atoms, i1, i2)
+    pos_diff = atoms[i1].position - atoms[i2].position
+    initial_dist = np.linalg.norm(pos_diff)
+
+    # operate on deepcopy to avoid changes to original object in memory
+    atoms = copy.deepcopy(atoms)
+    atoms_list = []
+    distance_list = []
+
+    # Retrieve previous constraints info
     if not atoms.constraints:
         initial_constraint = None
     else:
         initial_constraint = copy.deepcopy(atoms.constraints)
 
-    traj_counter = 0
-
-    # Write out the initial following naming scheme
-    traj_initial = Trajectory(str(traj_prefix) + str(traj_counter) + ".traj", 'w')
-    traj_initial.write(atoms)
-
-    for i in range(1, n_steps+1):
-        traj_counter += 1
-
+    for i in range(0, n_steps):
+        # operate on a deepcopy for intended functionality
+        atoms = copy.deepcopy(atoms)
         #remove previous contraints and set up new ones
         atoms.set_constraint()
-        new_constraint = FixBondLength(i1, i2)
         atoms.set_distance(i1, i2, (initial_dist + i * step_size), fix=0)
+
+        new_constraint = FixBondLength(i1, i2)
 
         if initial_constraint is not None:
             atoms.set_constraint([initial_constraint, new_constraint])
         else:
             atoms.set_constraint(new_constraint)
-        opt = BFGS(atoms, trajectory=traj_prefix+str(traj_counter)+".traj",
-                   restart=traj_prefix+str(traj_counter)+".pckl")
-        opt.run(fmax=fmax)
 
-    print("Have a nice day.")
+        atoms_list += [copy.deepcopy(atoms)]
+        distance_list += [initial_dist + i * step_size]
 
-def test_dissociation():
-    from ase.build import fcc100, add_adsorbate
-    from ase.calculators.emt import EMT
-    from ase import Atoms
-    slab = fcc100("Cu", (3,3,4), vacuum=10, periodic=True)
-    Au_adatoms = Atoms("2Au", positions=[(0,0,0), (2,0,0)])
-
-    add_adsorbate(slab, Au_adatoms, 2, position=(slab[18].x, slab[18].y))
-    slab.set_calculator(EMT())
-
-    # start with an optimised structure
-    from ase.optimize import BFGS
-    opt = BFGS(slab)
-    opt.run(fmax=0.05)
-
-    # move gold atoms away from each other
-    dissociation(slab, 36, 37, step_size=0.1)
-
-test_dissociation()
+    return atoms_list, distance_list
---
+++ examples/test_build_dissociation.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+'''
+Created on Fri 19/06/2020
+
+@author: Igor Kowalec
+
+Example use of a tool for investigating bond dissociation.
+Bond length of interest is fixed and is increased by step_size in each iteration.
+Returns a list of Atoms objects with changed positions and constraints applied,
+which can be optimised by the user - toy model of 2 Cu ad-atoms on Au surface.
+
+'''
+
+
+def test_dissociation():
+    from carmm.build.neb.bond_length_scan import dissociation
+    from ase.build import add_adsorbate
+    from examples.data.model_gen import get_example_slab
+    from ase import Atoms
+    from ase.calculators.emt import EMT
+    from ase.optimize import BFGS
+
+    # Generate and optimise slab prior to dissociation
+    slab = get_example_slab()
+    adatoms = Atoms("2Cu", positions=[(0,0,0), (2,0,0)])
+    add_adsorbate(slab, adatoms, height=2, position=(slab[0].x, slab[0].y))
+    opt = BFGS(slab)
+    opt.run(fmax=0.05)
+
+    # move adsorbed atoms (indices 18,19) away from each other
+    atoms_list, distance_list = dissociation(slab, 18, 19, step_size=0.2, n_steps=10)
+
+
+    for atoms in atoms_list:
+        atoms.set_calculator(EMT())
+        opt = BFGS(atoms)
+        opt.run(fmax=0.05)
+
+    from ase.visualize import view
+    view(atoms_list)
+    #print(distance_list)
+
+test_dissociation()
