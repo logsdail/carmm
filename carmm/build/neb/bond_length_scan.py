25c46dbd33b411d3259adca8920647b318a2d0b9
ikowalec <aikey2011@gmail.com>
2020-06-29 12:15:28 +0100

    Added a final_distance variable.

--- carmm/build/neb/bond_length_scan.py
+++ carmm/build/neb/bond_length_scan.py
@@ -4,7 +4,7 @@ Created on Fri 19/06/2020
 @author: Igor Kowalec, David Willock
 '''
 
-def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, reverse=False):
+def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, reverse=False, final_distance=None):
 
     '''
     This function is a tool for investigating bond dissociation.
@@ -23,9 +23,12 @@ def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, reverse=False):
         step_size: float
             Distance moved during dissociation in Angstrom per iteration
         n_steps: int
-            Total number of steps, including initial
+            Total number of steps
         reverse: boolean
             if True allows to examine association instead
+        final_distance: float
+            User can specify the final distance, the increments will be then based
+            on a fraction of n_steps/final_distance instead of step_size
     '''
 
     from ase.constraints import FixBondLength
@@ -45,18 +48,30 @@ def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, reverse=False):
     else:
         initial_constraint = copy.deepcopy(atoms.constraints)
 
-    for i in range(0, n_steps):
+    for i in range(1, n_steps+1):
         # operate on a deepcopy for intended functionality
         atoms = copy.deepcopy(atoms)
         # remove previous contraints and set up new ones
         atoms.set_constraint()
+
+        # move atoms and fix bond length in fixed increments or fraction of final_distance
+        # For reverse, reduce the distance rather than increase (association)
         if reverse is False:
-            atoms.set_distance(i1, i2, (initial_dist + i * step_size), fix=0)
-        elif reverse is True:
-            atoms.set_distance(i1, i2, (initial_dist - i * step_size), fix=0)
+            if final_distance:
+                measured_distance = initial_dist + (
+                    i/n_steps * (final_distance - initial_dist))
+            else:
+                measured_distance = (initial_dist + i * step_size)
+        elif reverse if True:
+            if final_distance:
+                measured_distance = (initial_dist - (
+                    i/n_steps * (final_distance - initial_dist)))
+            else:
+                measured_distance = (initial_dist - i * step_size)
 
+        atoms.set_distance(i1, i2, measured_distance, fix=0)
 
-
+        # adjust contraints
         if initial_constraint is not None:
             new_constraint = initial_constraint + [FixBondLength(i1, i2)]
             atoms.set_constraint(new_constraint)
@@ -64,9 +79,11 @@ def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, reverse=False):
             new_constraint = FixBondLength(i1, i2)
             atoms.set_constraint(new_constraint)
 
+        # Record the size of fixed bond
         atoms_list += [copy.deepcopy(atoms)]
         if reverse is False:
-            distance_list += [initial_dist + i * step_size]
+            distance_list += measured_distance
         elif reverse is True:
-            distance_list += [initial_dist - i * step_size]
+            distance_list += measured_distance
+
     return atoms_list, distance_list
