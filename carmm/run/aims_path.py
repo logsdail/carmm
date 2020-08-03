aaf834f9f27b034776d80fc4998f315630d3260d
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2020-08-03 11:50:25 +0100

    Added Thomas to CARMM runtime parameters

--- carmm/run/aims_path.py
+++ carmm/run/aims_path.py
@@ -16,6 +16,7 @@ def set_aims_command(hpc='hawk', basis_set='light'):
 
     mpirun = "time mpirun -np $SLURM_NTASKS "
     aprun = "time aprun -n $NPROCS "
+    gerun = "gerun"
     executable = "bin/aims.$VERSION.scalapack.mpi.x"
     species = "species_defaults/"+basis_set
 
@@ -28,6 +29,11 @@ def set_aims_command(hpc='hawk', basis_set='light'):
     elif hpc.lower() == 'archer':
         fhi_aims_directory="/home3/e05/e05/ajl340/fhi-aims-src-intel/"
         preamble = aprun
+    elif hpc.lower() == 'thomas':
+        fhi_aims_directory="/home/mmm0170/Software/fhi-aims/"
+        preamble = gerun
+    else:
+        raise Exception("Inappropriate HPC facility: " + hpc + "is not recognised")
 
     os.environ["ASE_AIMS_COMMAND"]= preamble + fhi_aims_directory + executable
     os.environ["AIMS_SPECIES_DIR"] = fhi_aims_directory + species
