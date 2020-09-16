Wrong value was referenced when group_move. Now clashes should not occur

carmm/build/neb/bond_length_scan.py
@@ -100,7 +100,7 @@ def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, final_distance=None,
             # make sure atoms from a group do not clash into surface atoms
             # move towards the surface or away if necessary
             if group_move:
-                z_threshold_max = np.amax([atom.z for atom in atoms[group_move]])
+                z_threshold_max = np.amin([atom.z for atom in atoms[group_move]])
             else:
                 z_threshold_max = atoms[i2].z
 