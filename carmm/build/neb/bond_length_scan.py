4c4e5ae6dac86addf391ae257b89ed8b466cd375
ikowalec <aikey2011@gmail.com>
2020-09-16 15:21:12 +0100

    Added a z-bias flag for atoms to prefer staying close to the surface

--- carmm/build/neb/bond_length_scan.py
+++ carmm/build/neb/bond_length_scan.py
@@ -4,7 +4,7 @@ Created on Fri 19/06/2020
 @author: Igor Kowalec, David Willock
 '''
 
-def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, final_distance=None, group_move=None):
+def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, final_distance=None, group_move=None, z_bias=False):
 
     '''
     This function is a tool for investigating bond dissociation.
@@ -32,15 +32,39 @@ def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, final_distance=None,
         group_move: list of integers
             User can specify a list of indices of atoms that need to be moved
             together with atom with index i2, e.g. OH group etc.
+        z_bias: boolean
+            TODO: Should this be a boolean or let user choose target z-coordinate?
+            WARNING - If TRUE this will make steps vary from defined step_size!
+            Bias to adjust the Z-coordinate of atom/group moving to approach
+            the surface in periodic calculations rather than just elongate the
+            bond.
     '''
 
     from ase.constraints import FixBondLength
     import copy
     import numpy as np
 
-    # retrieve initial atom - atom distance
+    # retrieve initial atom - atom distance and atom[i2] position
     pos_diff = atoms[i1].position - atoms[i2].position
     initial_dist = np.linalg.norm(pos_diff)
+    initial_i2_pos = copy.deepcopy(atoms[i2].position)
+
+    # retrieve z-coordinate for z_bias
+    if z_bias:
+        # make sure it works if no tags are set for atoms
+        # should be more reliable if available
+        surf_z_list = [atom.z for atom in atoms if atom.tag > 0]
+        if surf_z_list == []:
+            # Define a list of atom chemical symbols and their count
+            chem_symbol_count = [[x, atoms.get_chemical_symbols().count(x)] for x in set(atoms.get_chemical_symbols())]
+            # Sort the list by the count
+            def take_second(n):
+                return n[1]
+            chem_symbol_count.sort(key=take_second)
+            #print(chem_symbol_count)
+            surf_z_list = [atom.z for atom in atoms if atom.symbol == chem_symbol_count[-1][0]]
+        # The maximum z-coordinate of the surface atoms is retrieved
+        surf_z = np.amax(surf_z_list)
 
     atoms_list = []
     distance_list = []
@@ -68,6 +92,24 @@ def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, final_distance=None,
                 i/n_steps * (final_distance - initial_dist))
 
         atoms.set_distance(i1, i2, measured_distance, fix=0)
+
+        # apply bias in z-coordinate towards the surface atoms
+        # TODO: consult minimum distance from surface (can cause trouble for group)
+        z_threshold_min = surf_z + 2.0 # min distance in Angstrom from surf atoms
+        if z_bias:
+            # make sure atoms from a group do not clash into surface atoms
+            # move towards the surface or away if necessary
+            if group_move:
+                z_threshold_max = np.amax([atom.z for atom in atoms[group_move]])
+            else:
+                z_threshold_max = atoms[i2].z
+
+
+            if z_threshold_max > z_threshold_min:
+                atoms[i2].z -= (initial_i2_pos[2] - surf_z)/n_steps
+            elif z_threshold_max < z_threshold_min:
+                atoms[i2].z += (initial_i2_pos[2] - surf_z)/n_steps
+
         # move other specified atoms as part of a molecule, e.g. OH group
         if group_move:
             for m in group_move:
@@ -84,6 +126,6 @@ def dissociation(atoms, i1, i2, step_size=0.05, n_steps=20, final_distance=None,
 
         # Record the size of fixed bond
         atoms_list += [copy.deepcopy(atoms)]
-        distance_list += [measured_distance]
+        distance_list += [np.linalg.norm(atoms[i1].position - atoms[i2].position)]
 
     return atoms_list, distance_list
