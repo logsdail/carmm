5f415e154d52ae3da4359d7f36a22cee4397e48f
owainbeynon <54601295+owainbeynon@users.noreply.github.com>
2020-10-25 19:00:03 +0000

    Update distribution_functions.py
    Added a function that averages and plots the RDF of the last ten steps of an MD trajectory

--- carmm/analyse/distribution_functions.py
+++ carmm/analyse/distribution_functions.py
@@ -79,3 +79,117 @@ def radial_distribution_function(model, radius, position):
     plt.title('Radial Distribution Function', fontsize=15)
     pl.show()
     plt.show()
+
+def average_distribution_function(trajectory):
+
+   '''plots the average distribution function of the last 10 steps of an MD trajectory
+
+   TODO: -OB: looks pretty complicated with all the stored variables, can this be done in a loop?
+               All the different functions have a plot at the end, create a function that plots and can just be added '''
+
+
+
+    from ase.io import read
+    from matplotlib import pyplot as plt
+    from math import ceil
+    import numpy as np
+    import pylab as pl
+    import distance_distribution_function as ddf
+
+    # Variable Naming:
+    # [a-l] - Snapshot
+    # [a-l]d - Distance distribution of the snapshot
+    # [a-l]ds - Distance distribution array sorted from lowest-highest
+
+    bin_sampling = 0.1
+
+    a = read(trajectory, -1)
+    b = read(trajectory, -2)
+    c = read(trajectory, -3)
+    d = read(trajectory, -4)
+    e = read(trajectory, -5)
+    f = read(trajectory, -6)
+    g = read(trajectory, -7)
+    h = read(trajectory, -8)
+    l = read(trajectory, -9)
+    j = read(trajectory, -10)
+
+    del a.constraints
+    del b.constraints
+    del c.constraints
+    del d.constraints
+    del e.constraints
+    del f.constraints
+    del g.constraints
+    del h.constraints
+    del l.constraints
+    del j.constraints
+
+    ad = ddf(a)
+    bd = ddf(b)
+    cd = ddf(c)
+    dd = ddf(d)
+    ed = ddf(e)
+    fd = ddf(f)
+    gd = ddf(g)
+    hd = ddf(h)
+    ld = ddf(l)
+    jd = ddf(j)
+
+    ads = sorted(ad)
+    bds = sorted(bd)
+    cds = sorted(cd)
+    dds = sorted(dd)
+    eds = sorted(ed)
+    fds = sorted(fd)
+    gds = sorted(gd)
+    hds = sorted(hd)
+    lds = sorted(ld)
+    jds = sorted(jd)
+
+    mean = ads + bds + cds + dds + eds + fds + gds + hds + lds + jds
+    np.true_divide(mean, 10)
+
+    y, binEdges = np.histogram(ads, bins=ceil(max(ads) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', color="#808080")
+    y, binEdges = np.histogram(bds, bins=ceil(max(bds) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', color="#808080")
+    y, binEdges = np.histogram(cds, bins=ceil(max(cds) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', color="#808080")
+    y, binEdges = np.histogram(dds, bins=ceil(max(dds) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', color="#808080")
+    y, binEdges = np.histogram(eds, bins=ceil(max(eds) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', color="#808080")
+    y, binEdges = np.histogram(fds, bins=ceil(max(fds) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', color="#808080")
+    y, binEdges = np.histogram(gds, bins=ceil(max(gds) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', color="#808080")
+    y, binEdges = np.histogram(hds, bins=ceil(max(hds) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', color="#808080")
+    y, binEdges = np.histogram(lds, bins=ceil(max(lds) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', color="#808080")
+    y, binEdges = np.histogram(jds, bins=ceil(max(jds) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', color="#808080")
+    y, binEdges = np.histogram(mean, bins=ceil(max(mean) / bin_sampling), density=True)
+    bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+    pl.plot(bincenters, y, '-', label='Mean', color="#000000")
+
+    plt.xlabel('r/Å', fontsize=15)
+    plt.ylabel('g(r)', fontsize=15)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.title('Last 10 Snapshots', fontsize=15)
+    pl.legend()
+    pl.show()
+    plt.show()
+
