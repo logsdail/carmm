0e493a14af57cde15af16d95d207ce97cfc25caf
jack <Warrenje@cardiff.ac.uk>
2021-03-31 14:40:56 +0100

    Updated to hopefully make it a function and added comment to inform on what is going on

--- carmm/analyse/Plane.py
+++ carmm/analyse/Plane.py
@@ -1,67 +1,50 @@
-#def Calculate_planes(atoms, print_output=False);
+def get_lowest_distances(fn, A_mol, B_mol):
+    '''
+    Uses molecules.py to separate fn into molecules then measures the shortest distances between.
+    Indented for Periodic systems
 
-from typing import List
-from ase.io import read
-from ase.neighborlist import neighbor_list
-from ase import neighborlist
-import numpy as np
-from scipy import sparse
-from ase.visualize import view
-from ase.geometry.analysis import Analysis
-#from carmm.analyse.bonds import
+    Parameters:
+     fn : input file e.g. aims.out
 
+    Still very much a work in progress so go easy on it
+    '''
+    # Load Modules
+    from ase.io import read
+    import numpy as np
+    from ase.visualize import view
+    from ase.geometry.analysis import Analysis
+    # Read in input
+    atoms = read(filename)
 
+    # loads molecules function to detect discrete molecules in atoms object
+    analysis = Analysis(atoms)
+    from carmm.analyse.molecules import calculate_molecules
+    molecules = calculate_molecules(atoms)
 
-atoms = read("aims.out")
-analysis = Analysis(atoms)
+    # Gets all the distances in the model
+    distances = atoms.get_all_distances(mic=True, vector=False)
+    # Makes the molecules list into a *_mol atoms like object
+    A = molecules[0]
+    B = molecules[1]
 
-from carmm.analyse.molecules import calculate_molecules
+    A_mol = atoms[A]
+    B_mol = atoms[B]
+    #You can view these objects separated from the rest of your system
+    #view(*_mol)
 
+    # Loops measuring the shortest distance from every atom in A to B
+        measured = []
+        for a in A_mol:
+            bond_list_atom = []
+            for b in B_mol:
+                pos_diff = np.linalg.norm(a.position - b.position)
+                bond_list_atom += [pos_diff]
+            measured += [bond_list_atom]
+        lowest_distances = [np.amin(i) for i in measured]
 
-molecules = calculate_molecules(atoms)
-
-distances = atoms.get_all_distances(mic=True, vector=False)
-A = molecules[0]
-B = molecules[1]
-
-
-A_mol = atoms[A]
-B_mol = atoms[B]
-
-def get_lowest_distances(A_mol, B_mol):
-    '''Definition'''
-    measured = []
-    for a in A_mol :
-        bond_list_atom = []
-        for b in B_mol:
-            pos_diff = np.linalg.norm(a.position - b.position)
-            bond_list_atom+= [pos_diff]
-        measured+= [bond_list_atom]
-    lowest_distances = [np.amin(i) for i in measured]
-
-    return lowest_distances
+        return lowest_distances
 
 
 print(get_lowest_distances(A_mol, B_mol))
 
-#return print_AB, AB_Bonds, AB_BondsValues
-
-#print(atoms.symbols.get_chemical_formula())
-#print(molecules[0])
-#print(type(molecules[0]))
-#print(molecules.symbols.get_chemical_formula('hill', 'empirical'))
-
-# idx = 0
-# while idx <= 251:
-#    atomsIdx = component_list[idx]
-#    print("There are {} molecules in the system".format(n_components))
-#    print("Atom {} is part of molecule {}".format(idx, atomsIdx))
-#    atomsIdxs = [i for i in range(len(component_list)) if component_list[i] == atomsIdx]
-#   print("The following atoms are part of molecule {}: {}".format(atomsIdx, atomsIdxs))
-#    idx = idx + 1
-
-
 view(atoms)
-# for atom_index in range(0, len(atoms)):
-#    assert isinstance(offsets, object)
-#    print(atom_index, indices, offsets)
