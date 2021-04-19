3123c0a3f52cc391a585aac76f159b2fe7407ecd
jack <Warrenje@cardiff.ac.uk>
2021-04-19 13:11:58 +0100

    Included Andy's changes and TODO list

--- carmm/analyse/Plane.py
+++ carmm/analyse/Plane.py
@@ -1,49 +1,47 @@
+def get_interplane_distance(atoms):
+    '''
+    TODO: Document (@Jack Warren)
+    Returns:
+    '''
 
-
-    # Load Modules
-    import numpy as np
-    from ase.geometry.analysis import Analysis
-
-
+    #from ase.geometry.analysis import Analysis
     # loads molecules function to detect discrete molecules in atoms object
-    analysis = Analysis(atoms)
+    # AJL, Apr 2021: This unused - disabled for now. Remove?
+    #analysis = Analysis(atoms)
+
     from carmm.analyse.molecules import calculate_molecules
     molecules = calculate_molecules(atoms)
 
     # Makes the molecules list into a *_mol atoms like object
-    A = molecules[0]
-    B = molecules[1]
+    A_mol = atoms.copy()[molecules[0]]
+    B_mol = atoms.copy()[molecules[1]]
 
-    A_mol = atoms[A]
-    B_mol = atoms[B]
     #You can view these objects separated from the rest of your system using view(A_mol)
 
+    return get_lowest_distances(A_mol, B_mol)
 
-    def get_lowest_distances(A_mol, B_mol,):
-        '''
-        Uses molecules.py to separate fn into molecules then measures the shortest distances between.
-        Indented for Periodic systems
-
-        Parameters:
-         A_mol: Atoms object created by molecules
-         B_mol
-
-        Still very much a work in progress so go easy on it
-                '''
+def get_lowest_distances(A_mol, B_mol,):
+    '''
+    Uses molecules.py to separate fn into molecules then measures the shortest distances between.
+    Indented for Periodic systems
+    Parameters:
+    A_mol: Atoms object created by molecules
+    B_mol: Atoms object created by molecules
+    Returns:
+    Still very much a work in progress so go easy on it
+    '''
+    import numpy as np
 
     # Loops measuring the shortest distance from every atom in A to B
-        measured = []
-        for a in A_mol:
-            bond_list_atom = []
-            for b in B_mol:
-                pos_diff = np.linalg.norm(a.position - b.position)
-                bond_list_atom += [pos_diff]
-            measured += [bond_list_atom]
-        lowest_distances = [np.amin(i) for i in measured]
+    measured = []
+    for a in A_mol:
+        bond_list_atom = []
+        for b in B_mol:
+            pos_diff = np.linalg.norm(a.position - b.position)
+            bond_list_atom += [pos_diff]
+        measured += [bond_list_atom]
+    lowest_distances = [np.amin(i) for i in measured]
 
-        return lowest_distances
-
-
-print(get_lowest_distances(A_mol, B_mol))
-
-view(atoms)
+    # TODO: Document the return - this is a list of shortest distances from each atom in A
+    # TODO: What about information on the atoms that constitute the lowest distance?
+    return lowest_distances
