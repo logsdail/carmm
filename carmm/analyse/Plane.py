ce32ce5ed4488f2cb8abed3af6fa2bfd3ccd856d
jack <Warrenje@cardiff.ac.uk>
2021-04-28 14:11:34 +0100

    Added function for the establishment of a plane from 3 points

--- carmm/analyse/Plane.py
+++ carmm/analyse/Plane.py
@@ -45,3 +45,46 @@ def get_lowest_distances(A_mol, B_mol,):
     # TODO: Document the return - this is a list of shortest distances from each atom in A
     # TODO: What about information on the atoms that constitute the lowest distance?
     return lowest_distances
+
+def establish_planes(Atom1, Atom2, Atom3,):
+    '''
+    Using 3 points to calculate 3 vectors and establish a plane...
+
+    :param Atom1: (x,y,z coordinates of Atom1)
+    :param Atom2:
+    :param Atom3:
+    :return: Graphical Representation of planes
+    '''
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+
+    Atom1 = np.array([5,5,5])
+    Atom2 = np.array([0,-4,0])
+    Atom3 = np.array([0,0,1])
+
+    Vector1 = Atom1-Atom2
+    Vector2 = Atom2-Atom3
+    Vector3 = Atom1-Atom3
+
+# a plane is a*x+b*y+c*z+d=0
+# [a,b,c] is the normal. Thus, we have to calculate d and we're set
+
+    d1 = -np.sum(Atom1*Vector1) # dot product
+    d2 = -np.sum(Atom2*Vector2) # dot product
+    d3 = -np.sum(Atom3*Vector3) # dot product
+
+# create x,y
+    xx, yy = np.meshgrid(range(30), range(30))
+
+# calculate corresponding z
+    z1 = (-Vector1[0]*xx - Vector1[1]*yy - d1)*1./Vector1[2]
+    z2 = (-Vector2[0]*xx - Vector2[1]*yy - d2)*1./Vector2[2]
+    z3 = (-Vector3[0]*xx - Vector3[1]*yy - d3)*1./Vector3[2]
+
+# plot the surface
+    plt3d = plt.figure().gca(projection='3d')
+    plt3d.plot_surface(z1,z2,z3, color='blue')
+#    plt3d.plot_surface(xx,yy,z2, color='red')
+#    plt3d.plot_surface(xx,yy,z3, color='green')
+    plt.show()
