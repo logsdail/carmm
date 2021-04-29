0c6822c1b502a7beb445b24f0934d0654b107ef2
jack <Warrenje@cardiff.ac.uk>
2021-04-29 11:52:57 +0100

    removed the between

--- examples/analyse_center_of_masses_.py
+++ examples/analyse_center_of_masses_.py
@@ -4,7 +4,7 @@ Short example script to measure between the center of masses of two molecules
 
 
 
-def test_analyse_between_center_of_masses():
+def test_analyse_center_of_masses():
     from carmm.analyse.planes import center_of_mass_distance
     from ase.io import read
     from carmm.analyse.molecules import calculate_molecules
@@ -19,4 +19,4 @@ def test_analyse_between_center_of_masses():
     B_mol = atoms[B]
     center_of_mass_distance(A_mol, B_mol)
     print(center_of_mass_distance(A_mol, B_mol))
-test_analyse_between_center_of_masses()
+test_analyse_center_of_masses()
