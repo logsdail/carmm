1b06012b8f02584b7bf6731bbf268a12e1961881
Igor Kowalec <58031188+ikowalec@users.noreply.github.com>
2021-08-25 11:22:44 +0100

    removing redundancies

--- examples/analyse_neighbours.py
+++ examples/analyse_neighbours.py
@@ -7,9 +7,6 @@ def test_neighbours():
     from data.model_gen import get_example_slab as slab
     slab = slab(adsorbate=True)
 
-    from ase.visualize import view
-    view(slab)
-
     # Calculate neighbours
     all_neighbour_atoms, shell_list = neighbours(slab, [13], 1, verbose=True)
 
