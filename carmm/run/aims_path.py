5615a91828a82d286f64c92b623b7a5818c279b8
Igor Kowalec <58031188+ikowalec@users.noreply.github.com>
2021-09-16 12:43:11 +0100

    Added definition for the default variable and reusing existing if statements

--- carmm/run/aims_path.py
+++ carmm/run/aims_path.py
@@ -11,17 +11,15 @@ def set_aims_command(hpc='hawk', basis_set='light', defaults=2010):
     basis_set: String
         Name of basis set for FHI-aims
         Options: 'light', 'intermediate', 'tight', 'really_tight' etc.
-    standard: int
+    defaults: int
         Either 2010 or 2020 referring to the default species basis sets
-        that come with FHI-aims, which adhere to the year 2010 or 2020
-        standard. Old 2010 value by default to avoid disruption for users.
+        that come with new FHI-aims release, which adhere to the year 2010
+         or 2020 standard. Old 2010 value by default to avoid disruption
+         for users.
     '''
     import os
 
-    if defaults == 2010:
-        standard = "defaults_2010/"
-    elif defaults == 2020:
-        standard = "defaults_2020/"
+
 
     mpirun = "time mpirun -np $SLURM_NTASKS "
     aprun = "time aprun -n $NPROCS "
@@ -29,11 +27,7 @@ def set_aims_command(hpc='hawk', basis_set='light', defaults=2010):
     srun = "srun --cpu-bind=cores --distribution=block:block --hint=nomultithread "
     executable = "bin/aims.$VERSION.scalapack.mpi.x"
 
-    # account for the new layout of species_defaults on ARCHER2
-    if hpc.lower() == "archer2":
-        species = "species_defaults/" + standard + basis_set
-    else:
-        species = "species_defaults/" + basis_set
+    species = "species_defaults/" + basis_set
 
     if hpc.lower() == 'hawk':
         fhi_aims_directory="/apps/local/projects/scw1057/software/fhi-aims/"
@@ -45,6 +39,8 @@ def set_aims_command(hpc='hawk', basis_set='light', defaults=2010):
     elif hpc.lower() == 'archer2':
         fhi_aims_directory="/work/e05/e05-files-log/shared/software/fhi-aims/"
         preamble = srun
+        standard = "defaults_"+str(defaults)+"/"
+        species = "species_defaults/" + standard + basis_set
     #elif hpc.lower() == 'thomas': # Retired Oct 2020
     elif hpc.lower() == 'young':
         fhi_aims_directory="/home/mmm0170/Software/fhi-aims/"
