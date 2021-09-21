e408e6270cae12f80a7d3a265fc2b31d32453d4c
Igor Kowalec <58031188+ikowalec@users.noreply.github.com>
2021-09-21 10:32:56 +0100

    Now correctly consider constraints for forces convergence check

--- examples/analyse_forces.py
+++ examples/analyse_forces.py
@@ -14,24 +14,35 @@ def test_analyse_forces():
     from ase.calculators.emt import EMT
     from ase.optimize import BFGSLineSearch
     from carmm.analyse.forces import is_converged
+    from ase.constraints import FixAtoms
 
-    fmax = 0.05 # eV/Angstrom
-    atoms = molecule("H2")
+    fmax = 0.01 # eV/Angstrom
+    atoms = molecule("CO2")
     atoms.calc = EMT()
 
     # Returns False prior to optimisation (no forces in calculator)
     opt = BFGSLineSearch(atoms)
     print(is_converged(atoms, fmax))
     assert(is_converged(atoms, fmax) == False)
+    print(atoms.calc.forces)
 
     # Returns False if optimisation has not reached to/below fmax
     opt.run(fmax=fmax*30)
     print(is_converged(atoms, fmax))
     assert(is_converged(atoms, fmax) == False)
 
-    # Returns True if optimised to or below desired fmax
+    # Returns True if optimised to or below desired fmax with constraints
+    c = FixAtoms(indices=[0,1])
+    atoms.set_constraint(c)
+    opt.run(fmax=fmax)
+    print(is_converged(atoms, fmax))
+    assert (is_converged(atoms, fmax) == True)
+
+    # Returns True if optimised to or below desired fmax without constraints
+    atoms.set_constraint()
     opt.run(fmax=fmax)
     print(is_converged(atoms, fmax))
     assert(is_converged(atoms, fmax) == True)
+
 
 test_analyse_forces()
