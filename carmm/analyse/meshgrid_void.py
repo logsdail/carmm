9452162e8691009854aa976cc5a96fc4a6f6d268
Gabriel Bramley <BramleyG@cardiff.ac.uk>
2022-04-04 15:02:54 +0100

    - Changed logic of void definition - now calculates the radius of voids relative to atomic coordinates and plots separately. - distance_meshgrid2point fully vectorised. - Removed depreciated functions.

--- carmm/analyse/meshgrid_void.py
+++ carmm/analyse/meshgrid_void.py
@@ -1,74 +1,105 @@
-def void_mesh_build(void_min, void_max, ucell_obj, mol_xx, mic, coarseness=1):
-    '''
-    Defines the void (unoccupied regions) of a unit cell on a numpy meshgrid. Scans over probe points on the
-    underlying meshgrid by increasing the radius of the grid until the van der Waals' volume of a molecule is
-    encountered. Coarseness factor reduces number of probe points by skipping over points in the grid.
-    Uses 99.99 as a junk value.
+def void_find(ucell_obj, atom, mic=True, coarseness=1):
+    """
+    Defines the void centres and radii of points across the meshgrid. Coarseness factor reduces number of probe points
+    by skipping over points in the grid. Uses 99.99 as a junk value.
     Args:
-        void_min: float
-            Minimum radius scanned from for probe sphere.
-        void_max: float
-            Maximum radius scanned from for probe sphere.
         ucell_obj: unit_cell object
             Contains meshgrid and associated unit cell information.
-        mol_xx: numpy array [nx,ny,nz]
-            Grid to determine whether molecule is present at a point.
+        atom: Atoms object
+            Supplies coordinates and atomic information.
         mic: logical
-            Use miniumum image convention.
+            Use minimum image convention.
         coarseness: integer
             Skips over an integer number of points
     Return:
-        void_xx, void_yy, void_zz: Meshgrid of (x,y,z) occupied by the probe spheres.
-    '''
+        void_centres: numpy array
+            Atomic co-ordiantes of the void centres.
+        void_radii: numpy array
+            Radius of the void centres
+    """
 
     import numpy as np
-    from carmm.analyse.meshgrid_functions import distance_meshgrid2point
+    from ase.data.vdw_alvarez import vdw_radii
+    from ase.data import atomic_numbers
 
-    # Defines the mesh points occupied by atoms.
-    X0 = np.full(ucell_obj.nx, fill_value=99.99)
-    Y0 = np.full(ucell_obj.ny, fill_value=99.99)
-    Z0 = np.full(ucell_obj.nz, fill_value=99.99)
-    void_xx, void_yy, void_zz = np.meshgrid(X0, Y0, Z0, indexing='xy')
     void_centres = []
+    void_radii = []
+    atom_radii = []
 
-    # FIND UNOCCUPIED SITES
-    probe_xx = np.where(mol_xx == 99.99, ucell_obj.xx, 99.99)
+    for atom_co in range(len(atom.positions)):
+        at_symbols = atom.get_chemical_symbols()
+        at_n = atomic_numbers[at_symbols[atom_co]]
+        atom_radius = vdw_radii[at_n]
+        atom_radii.append(atom_radius)
 
-    mol_xx_mask = (mol_xx != 99.99)
+    atom_radii = np.array([atom_radii]).flatten()
 
     for i in range(0, ucell_obj.nx, coarseness):
         for j in range(0, ucell_obj.ny, coarseness):
             for k in range(0, ucell_obj.nz, coarseness):
 
-                if probe_xx[i, j, k] == 99.99:
-                    continue
-
                 a_xx, a_yy, a_zz = ucell_obj.xx[i, j, k], ucell_obj.yy[i, j, k], ucell_obj.zz[i, j, k]
 
-                point_accepted = False
-                for pbox_r in np.arange(void_min, void_max, 0.5):
-                    probe_box_distance_matrix = distance_meshgrid2point(a_xx, a_yy, a_zz, ucell_obj, mic)
+                distances = np.abs(atom.positions - np.array([a_xx, a_yy, a_zz]))
+                if mic:
+                    distances = np.where(distances > 0.5 * ucell_obj.dim, distances - ucell_obj.dim, distances)
 
-                    ping = probe_box_distance_matrix < pbox_r
+                distances = np.sqrt((distances ** 2).sum(axis=-1)) - atom_radii
 
-                    point_denied = np.any(ping & mol_xx_mask)
+                min_distance = np.amin(distances)
 
-                    if (not point_denied):
-                        point_accepted = True
+                void_centres.append([a_xx, a_yy, a_zz])
+                void_radii.append([min_distance])
 
-                    if point_denied and (not point_accepted):
-                        #print(f"Probe denied at: {a_xx,a_yy,a_zz} with radius: {pbox_r}")
-                        break
+    void_centres = np.array([void_centres])
+    void_centres = void_centres[0]
+    void_radii = np.array([void_radii]).flatten()
 
-                    if point_denied and point_accepted:
-                        #print(f"Probe accepted at: {a_xx,a_yy,a_zz} with radius: {pbox_r-0.5}")
-                        void_centres.append([a_xx, a_yy, a_zz])
+    return void_centres, void_radii
 
-                        ping = probe_box_distance_matrix < (pbox_r - 0.5)
 
-                        void_xx = np.where(ping, ucell_obj.xx, void_xx)
-                        void_yy = np.where(ping, ucell_obj.yy, void_yy)
-                        void_zz = np.where(ping, ucell_obj.zz, void_zz)
-                        break
+def void_build_mask(ucell, void_centres, void_radii, mic, min_void=1.0):
+    """
+    Defines meshgrid of voids given by list of void centres and their radii. Uses 99.99 as a junk value.
+    - Replace junk value with None??
+    Args:
+        ucell: unit_cell object
+            Meshgrid and unit cell information
+        void_centres: numpy array
+            Co-ordinates of the void centres.
+        void_radii: numpy array
+            Radius of the void sphere.
+        mic: logical
+            Minimum image convention for PBC on/off.
+        min_void: float
+            Defines the minimum size of void to plot.
+    Returns:
+        void_xx, void_yy, void_zz: numpy array
+            Contains values of (x, y, z) co-ordinates for each point on axis occupied by
+            atom van der Waals volume. Set to junk value otherwise.
+    """
+
+    import numpy as np
+    from carmm.analyse.meshgrid_functions import distance_meshgrid2point
+
+    # Defines the mesh points occupied by atoms. Uses 99.99 as a trash value.
+    X0 = np.full(ucell.nx, fill_value=99.99)
+    Y0 = np.full(ucell.ny, fill_value=99.99)
+    Z0 = np.full(ucell.nz, fill_value=99.99)
+
+    void_xx, void_yy, void_zz = np.meshgrid(X0, Y0, Z0, indexing='xy')
+
+    for i in range(len(void_centres)):
+
+        if void_radii[i] < min_void:
+            continue
+
+        a_xx, a_yy, a_zz = void_centres[i][0], void_centres[i][1], void_centres[i][2]
+
+        new_distances = distance_meshgrid2point(a_xx, a_yy, a_zz, ucell.xx, ucell.yy, ucell.zz, ucell.dim, mic)
+
+        void_xx = np.where(new_distances < void_radii[i], ucell.xx, void_xx)
+        void_yy = np.where(new_distances < void_radii[i], ucell.yy, void_yy)
+        void_zz = np.where(new_distances < void_radii[i], ucell.zz, void_zz)
 
     return void_xx, void_yy, void_zz
