fd78a1620f30492701e45c6360d86f8f4f86622e
Gabriel Bramley <BramleyG@cardiff.ac.uk>
2022-05-02 17:07:50 +0100

    (1) Fixed silly bug (3).

--- examples/analyse_meshgrid_voidanalysis_MFI.py
+++ examples/analyse_meshgrid_voidanalysis_MFI.py
@@ -20,26 +20,26 @@ def test_void_find():
     from ase.io import read
 
     # Initialise the Mesh object, with a default underlying meshgrid of 50x50x50 grid points.
-    MFI_Mesh = Mesh(np.array([20.22614449,19.82125040,13.36948553,90,90,90]),pbc=[1,1,1])
+    mfi_mesh = Mesh(np.array([20.22614449, 19.82125040, 13.36948553, 90, 90, 90]), pbc=[1, 1, 1])
 
     # Read atomic coordinates and create a meshgrid using periodic boundary conditions
     # Meshgrid arrays print the x, y, z coordinates for all given points into three (nx,ny,nz)
     # arrays (xx, yy and zz respectively).
     atom = read('data/Zeolite/MFI_framework_geom.xyz')
-    mol_xx, mol_yy, mol_zz = atom_mesh_build_mask(MFI_Mesh, atom)
+    mol_xx, mol_yy, mol_zz = atom_mesh_build_mask(mfi_mesh, atom)
 
     # First find the maximum volume sphere not containing an atom around each grid (probe) point.
     # Coarseness controls the number of probe points by increasing the interval between each probe point.
     # Outputs a list of the void center coordinates and their maximum radii.
-    void_centers, void_radii = void_find(MFI_Mesh, atom, coarseness=2)
+    void_centers, void_radii = void_find(mfi_mesh, atom, coarseness=2)
 
     # Plot the void centers onto the grid. Min_void prevents spheres of radii smaller than a given
     # value being plotted.
-    void_xx,void_yy,void_zz=void_build_mask(MFI_Mesh, void_centers, void_radii, min_void=1.4)
+    void_xx,void_yy,void_zz=void_build_mask(mfi_mesh, void_centers, void_radii, min_void=1.4)
 
     # Print basic output details such as the total void volume and the maximum void volume, radius and position.
     # Only requires one void meshgrid to determine whether a site is unoccupied/occupied.
-    void_analysis(MFI_Mesh, void_centers, void_radii, void_xx)
+    void_analysis(mfi_mesh, void_centers, void_radii, void_xx)
 
     # Plots the meshgrid for the molecules and void.
     fig = plt.figure(figsize=(10,10))
@@ -48,9 +48,9 @@ def test_void_find():
     ax.scatter(mol_xx,mol_yy,mol_zz)
     ax.scatter(void_xx,void_yy,void_zz)
 
-    ax.set_xlim(0,Meshgrid.x_max)
-    ax.set_ylim(0,Meshgrid.y_max)
-    ax.set_zlim(0,Meshgrid.z_max)
+    ax.set_xlim(0, mfi_mesh.x_max)
+    ax.set_ylim(0, mfi_mesh.y_max)
+    ax.set_zlim(0, mfi_mesh.z_max)
     plt.show()
 
 test_void_find()
