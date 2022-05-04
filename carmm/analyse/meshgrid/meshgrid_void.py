d828c3d6ef5cff26b055d4599118b571034d04ce
Gabriel Bramley <BramleyG@cardiff.ac.uk>
2022-05-04 11:48:49 +0100

    - Committed changes suggested by Igor.

--- carmm/analyse/meshgrid/meshgrid_void.py
+++ carmm/analyse/meshgrid/meshgrid_void.py
@@ -1,11 +1,13 @@
-def void_find(MeshObject, Atom, coarseness=1):
+def void_find(meshobject, atoms, coarseness=1):
     """
-    Defines the void centres and radii of points across the meshgrid. Coarseness factor reduces number of probe points
-    by skipping over points in the grid. Uses 99.99 as a junk value.
+    Defines the maximum spherical radius of probes in unoccupied space. Scans through the distance
+    between the probe point and all atoms in the system, and defines the maximum radius of the probe
+    as the smallest value of (probe_position - atom_position - vdw_radii).
+    Coarseness factor reduces number of probe points by skipping over points in the grid.
     Args:
-        MeshObject: MeshgridObject object
+        meshobject: Mesh object
             Contains meshgrid and associated unit cell information.
-        Atom: Atoms object
+        atoms: Atoms object
             Supplies coordinates and atomic information.
         coarseness: integer
             Skips over an integer number of points
@@ -23,18 +25,18 @@ def void_find(MeshObject, Atom, coarseness=1):
     void_centres = []
     void_radii = []
 
-    atom_radii = vdw_radii[Atom.numbers]
+    atom_radii = vdw_radii[atoms.numbers]
 
     atom_radii = np.array([atom_radii]).flatten()
 
-    for i in range(0, MeshObject.nx, coarseness):
-        for j in range(0, MeshObject.ny, coarseness):
-            for k in range(0, MeshObject.nz, coarseness):
+    for i in range(0, meshobject.nx, coarseness):
+        for j in range(0, meshobject.ny, coarseness):
+            for k in range(0, meshobject.nz, coarseness):
 
-                a_xx, a_yy, a_zz = MeshObject.xx[i, j, k], MeshObject.yy[i, j, k], MeshObject.zz[i, j, k]
+                a_xx, a_yy, a_zz = meshobject.xx[i, j, k], meshobject.yy[i, j, k], meshobject.zz[i, j, k]
 
-                distances = get_distances(np.array([a_xx, a_yy, a_zz]), p2=Atom.positions,
-                                       cell=MeshObject.Cell, pbc=MeshObject.pbc)
+                distances = get_distances(np.array([a_xx, a_yy, a_zz]), p2=atoms.positions,
+                                          cell=meshobject.Cell, pbc=meshobject.pbc)
 
                 distances = distances[1] - atom_radii
 
@@ -50,19 +52,16 @@ def void_find(MeshObject, Atom, coarseness=1):
     return void_centres, void_radii
 
 
-def void_build_mask(MeshObject, void_centres, void_radii, min_void=1.0):
+def void_build_mask(meshobject, void_centres, void_radii, min_void=1.0):
     """
-    Defines meshgrid of voids given by list of void centres and their radii. Uses 99.99 as a junk value.
-    - Future change - replace junk value with None??
+    Defines meshgrid of voids given by list of void centres and their radii. Uses np.nan as a junk value.
     Args:
-        Ucell: unit_cell object
-            Meshgrid and unit cell information
+        meshobject: Mesh object.
+            Meshgrid and unit cell information.
         void_centres: numpy array
             Coordinates of the void centres.
         void_radii: numpy array
             Radius of the void sphere.
-        mic: logical
-            Minimum image convention for PBC on/off.
         min_void: float
             Defines the minimum size of void to plot.
     Returns:
@@ -74,10 +73,10 @@ def void_build_mask(MeshObject, void_centres, void_radii, min_void=1.0):
     import numpy as np
     from carmm.analyse.meshgrid.meshgrid_functions import distance_meshgrid2point
 
-    # Defines the mesh points occupied by atoms. Uses 99.99 as a trash value.
-    x0 = np.full(MeshObject.nx, fill_value=99.99)
-    y0 = np.full(MeshObject.ny, fill_value=99.99)
-    z0 = np.full(MeshObject.nz, fill_value=99.99)
+    # Defines the mesh points occupied by atoms. Uses np.nan as a trash value.
+    x0 = np.full(meshobject.nx, fill_value=np.nan)
+    y0 = np.full(meshobject.ny, fill_value=np.nan)
+    z0 = np.full(meshobject.nz, fill_value=np.nan)
 
     void_xx, void_yy, void_zz = np.meshgrid(x0, y0, z0, indexing='xy')
 
@@ -88,28 +87,30 @@ def void_build_mask(MeshObject, void_centres, void_radii, min_void=1.0):
 
         a_xx, a_yy, a_zz = void_centres[i][0], void_centres[i][1], void_centres[i][2]
 
-        new_distances = distance_meshgrid2point(a_xx, a_yy, a_zz, MeshObject)
+        new_distances = distance_meshgrid2point(a_xx, a_yy, a_zz, meshobject)
 
-        void_xx = np.where(new_distances < void_radii[i], MeshObject.xx, void_xx)
-        void_yy = np.where(new_distances < void_radii[i], MeshObject.yy, void_yy)
-        void_zz = np.where(new_distances < void_radii[i], MeshObject.zz, void_zz)
+        void_xx = np.where(new_distances < void_radii[i], meshobject.xx, void_xx)
+        void_yy = np.where(new_distances < void_radii[i], meshobject.yy, void_yy)
+        void_zz = np.where(new_distances < void_radii[i], meshobject.zz, void_zz)
 
     return void_xx, void_yy, void_zz
 
 
-def void_analysis(MeshObject, void_centres, void_radii, void_xx):
+def void_analysis(meshobject, void_centres, void_radii, void_xx):
     """
-    Defines meshgrid of voids given by list of void centres and their radii. Uses 99.99 as a junk value.
-    - Replace junk value with None??
+    Prints basic volumentric information of the void.
     Args:
-        Ucell: unit_cell object
-            Meshgrid and unit cell information
+        meshobject: Mesh object.
+            Meshgrid and unit cell information.
         void_centres: numpy array
             Coordinates of the void centres.
         void_radii: numpy array
             Radius of the void sphere.
         void_xx: meshgrid numpy array
             x coordinates of void mesh grid.
+    Returns:
+        void_volume: float
+            The total volume of the volume accessible to the probe (Ang**3).
     """
 
     import numpy as np
@@ -121,10 +122,10 @@ def void_analysis(MeshObject, void_centres, void_radii, void_xx):
     unique, counts = np.unique(void_xx, return_counts=True)
     counter = counts[-1]
 
-    volume = MeshObject.Cell.volume
+    volume = meshobject.Cell.volume
 
     unocc_sites = np.size(void_xx) - counter
-    vox_volume = volume / (MeshObject.nx * MeshObject.ny * MeshObject.nz)
+    vox_volume = volume / (meshobject.nx * meshobject.ny * meshobject.nz)
 
     void_volume = unocc_sites * vox_volume
 
--- examples/analyse_meshgrid_voidanalysis_MFI.py
+++ examples/analyse_meshgrid_voidanalysis_MFI.py
@@ -19,41 +19,53 @@ def test_void_find():
     import numpy as np
     from ase.io import read
 
-    # Initialise the Mesh object, with a default underlying meshgrid of 50x50x50 grid points.
+    # Initialise the Mesh object, with a default underlying meshgrid of (nx, ny, nz) grid points.
     mfi_mesh = Mesh(np.array([20.22614449, 19.82125040, 13.36948553, 90, 90, 90]),
                     nx = 20, ny = 20, nz = 20, pbc=[1, 1, 1])
 
-    # Read atomic coordinates and create a meshgrid using periodic boundary conditions
+
+    # Read xyz file into Atoms object.
+    atoms = read('data/Zeolite/MFI_framework_geom.xyz')
+
     # Meshgrid arrays print the x, y, z coordinates for all given points into three (nx,ny,nz)
     # arrays (xx, yy and zz respectively).
-    atom = read('data/Zeolite/MFI_framework_geom.xyz')
-    mol_xx, mol_yy, mol_zz = atom_mesh_build_mask(mfi_mesh, atom)
+    mol_xx, mol_yy, mol_zz = atom_mesh_build_mask(mfi_mesh, atoms)
 
     # First find the maximum volume sphere not containing an atom around each grid (probe) point.
     # Coarseness controls the number of probe points by increasing the interval between each probe point.
     # Outputs a list of the void center coordinates and their maximum radii.
-    void_centers, void_radii = void_find(mfi_mesh, atom, coarseness=2)
+    void_centers, void_radii = void_find(mfi_mesh, atoms, coarseness=2)
 
     # Plot the void centers onto the grid. Min_void prevents spheres of radii smaller than a given
     # value being plotted.
-    void_xx,void_yy,void_zz=void_build_mask(mfi_mesh, void_centers, void_radii, min_void=1.4)
+    void_xx,void_yy,void_zz= void_build_mask(mfi_mesh, void_centers, void_radii, min_void=1.4)
 
     # Print basic output details such as the total void volume and the maximum void volume,
     # radius and position.
     # Only requires one void meshgrid to determine whether a site is unoccupied/occupied.
     periodic_volume = void_analysis(mfi_mesh, void_centers, void_radii, void_xx)
 
+    # Test the overall PBC distances on the meshgrid are small than OBC.
     mfi_mesh_obc = Mesh(np.array([20.22614449, 19.82125040, 13.36948553, 90, 90, 90]),
                         nx = 20, ny = 20, nz = 20, pbc=[0, 0, 0])
 
-    test_x, test_y, test_z = atom.positions[0][0], atom.positions[0][1], atom.positions[0][2] 
+    test_x, test_y, test_z = atoms.positions[0][0], atoms.positions[0][1], atoms.positions[0][2]
     periodic_dist = distance_meshgrid2point(test_x, test_y, test_z, mfi_mesh)
     obc_dist      = distance_meshgrid2point(test_x, test_y, test_z, mfi_mesh_obc)
-
-    print(np.mean(periodic_dist))
-    print(np.mean(obc_dist))
     
     assert(np.mean(periodic_dist) < np.mean(obc_dist))
     assert(np.isclose(periodic_volume, 890.4178197211703, atol=1e-4, rtol=0.0))
+
+    # Plots the meshgrid for the molecules and void.
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(mol_xx,mol_yy,mol_zz)
+    ax.scatter(void_xx,void_yy,void_zz)
+
+    ax.set_xlim(0, mfi_mesh.x_max)
+    ax.set_ylim(0, mfi_mesh.y_max)
+    ax.set_zlim(0, mfi_mesh.z_max)
+    plt.show()
 
 test_void_find()
