a06e2ae71a6c0c293d27b142810f9fe7d925243f
Jack Warren <warrenje@cardiff.ac.uk>
2022-06-22 11:54:55 +0100

    Tweeked some errors introduced with the name change

--- carmm/analyse/planes.py
+++ carmm/analyse/planes.py
@@ -10,7 +10,6 @@ def get_interplane_distances(atoms):
     # loads molecules function to detect discrete molecules in atoms object
     # AJL, Apr 2021: This unused - disabled for now. Remove?
 
-
     from carmm.analyse.molecules import calculate_molecules
     molecules = calculate_molecules(atoms)
 
@@ -84,42 +83,42 @@ def establish_planes(Atom1, Atom2, Atom3):
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
 
-    Atom1 = np.array([5,5,5])
-    Atom2 = np.array([0,-4,0])
-    Atom3 = np.array([0,0,1])
+    Atom1 = np.array([5, 5, 5])
+    Atom2 = np.array([0, -4, 0])
+    Atom3 = np.array([0, 0, 1])
 
-    Vector1 = Atom1-Atom2
-    Vector2 = Atom2-Atom3
-    Vector3 = Atom1-Atom3
+    Vector1 = Atom1 - Atom2
+    Vector2 = Atom2 - Atom3
+    Vector3 = Atom1 - Atom3
 
-# a plane is a*x+b*y+c*z+d=0
-# [a,b,c] is the normal. Thus, we have to calculate d and we're set
+    # a plane is a*x+b*y+c*z+d=0
+    # [a,b,c] is the normal. Thus, we have to calculate d and we're set
 
-    d1 = -np.sum(Atom1*Vector1) # dot product
-    d2 = -np.sum(Atom2*Vector2) # dot product
-    d3 = -np.sum(Atom3*Vector3) # dot product
+    d1 = -np.sum(Atom1 * Vector1)  # dot product
+    d2 = -np.sum(Atom2 * Vector2)  # dot product
+    d3 = -np.sum(Atom3 * Vector3)  # dot product
 
-# create x,y
+    # create x,y
     xx, yy = np.meshgrid(range(30), range(30))
 
-# calculate corresponding z
-    z1 = (-Vector1[0]*xx - Vector1[1]*yy - d1)*1./Vector1[2]
-    z2 = (-Vector2[0]*xx - Vector2[1]*yy - d2)*1./Vector2[2]
-    z3 = (-Vector3[0]*xx - Vector3[1]*yy - d3)*1./Vector3[2]
+    # calculate corresponding z
+    z1 = (-Vector1[0] * xx - Vector1[1] * yy - d1) * 1. / Vector1[2]
+    z2 = (-Vector2[0] * xx - Vector2[1] * yy - d2) * 1. / Vector2[2]
+    z3 = (-Vector3[0] * xx - Vector3[1] * yy - d3) * 1. / Vector3[2]
 
-# plot the surface
+    # plot the surface
     plt3d = plt.figure().gca(projection='3d')
-    plt3d.plot_surface(z1,z2,z3, color='blue')
-#    plt3d.plot_surface(xx,yy,z2, color='red')
-#    plt3d.plot_surface(xx,yy,z3, color='green')
-#    plt.show()
-   
+    plt3d.plot_surface(z1, z2, z3, color='blue')
+    #    plt3d.plot_surface(xx,yy,z2, color='red')
+    #    plt3d.plot_surface(xx,yy,z3, color='green')
+    #    plt.show()
+
     # Adding the appropriate return
     return plt
 
+
 def plane_of_best_fit(model):
-    
-    ''' Using the xyz coordinates of a molecule or atoms object to calculate the plane of best fit and plot it for visualizing 
+    ''' Using the xyz coordinates of a molecule or atoms object to calculate the plane of best fit and plot it for visualizing
     model: Atoms object
         - The atoms object that has been *previously* loaded (note the IO is not done here)
 
@@ -168,14 +167,14 @@ def plane_of_best_fit(model):
     # plot plane
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
-    X,Y = np.meshgrid(np.arange(xlim[0], xlim[1]),
-                    np.arange(ylim[0], ylim[1]))
+    X, Y = np.meshgrid(np.arange(xlim[0], xlim[1]),
+                       np.arange(ylim[0], ylim[1]))
     Z = np.zeros(X.shape)
     for r in range(X.shape[0]):
         for c in range(X.shape[1]):
-            Z[r,c] = fit[0] * X[r,c] + fit[1] * Y[r,c] + fit[2]
+            Z[r, c] = fit[0] * X[r, c] + fit[1] * Y[r, c] + fit[2]
 
-    ax.plot_wireframe(X,Y,Z, color='k')
+    ax.plot_wireframe(X, Y, Z, color='k')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
