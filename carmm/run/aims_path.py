d6c61a81c90b4548eeb7de9ccaf5120e570442bb
Igor Kowalec <58031188+ikowalec@users.noreply.github.com>
2022-07-12 21:51:27 +0100

    Reverted dictionary names and defined task_farmed_command regardless of npi setting

--- carmm/run/aims_path.py
+++ carmm/run/aims_path.py
@@ -19,42 +19,40 @@ def set_aims_command(hpc='hawk', basis_set='light', defaults=2010, nodes_per_ins
     """
     import os
 
-    preamble_types = {
+    hpc = hpc.lower()
+
+    preamble = {
         "hawk": "time mpirun -np $SLURM_NTASKS ",
         "isambard": "time aprun -n $NPROCS ",
         "archer2": "srun --cpu-bind=cores --distribution=block:block --hint=nomultithread ",
         "young": "gerun "
     }
 
-    hpc = hpc.lower()
-    assert hpc in preamble_types, "Inappropriate HPC facility: " + hpc + "is not recognised."
+    assert hpc in preamble, "Inappropriate HPC facility: " + hpc + "is not recognised."
 
-    fhi_aims_directories = {
+    fhi_aims_directory = {
         "hawk": "/apps/local/projects/scw1057/software/fhi-aims/",
         "isambard": "/home/ca-alogsdail/fhi-aims-gnu/",
         "archer2": "/work/e05/e05-files-log/shared/software/fhi-aims/",
         "young": "/home/mmm0170/Software/fhi-aims/",
     }
 
-    executable = "bin/aims.$VERSION.scalapack.mpi.x"
-
-    species = "species_defaults/" + "defaults_" + str(defaults) + "/" + basis_set
-
-    preamble = preamble_types[hpc]
-    fhi_aims_directory = fhi_aims_directories[hpc]
-
-    if nodes_per_instance:
-        assert hpc in ["archer2", "hawk"], "Only ARCHER2 and Hawk supported for task-farming at the moment."
-
-        task_farmed_commands = {
+    task_farmed_commands = {
             "archer2": "--nodes=" + str(nodes_per_instance) + " --ntasks=" + str(int(128 * nodes_per_instance)) + " ",
             "hawk": "--nodes=" + str(nodes_per_instance) + " --ntasks=" + str(int(40 * nodes_per_instance)) + " ",
             # TODO: add and test isambard and young task-farmed commands
             "isambard": "",
             "young": "",
-        }
+    }
 
-        preamble += task_farmed_commands[hpc]
+    executable = "bin/aims.$VERSION.scalapack.mpi.x"
 
-    os.environ["ASE_AIMS_COMMAND"] = preamble + fhi_aims_directory + executable
-    os.environ["AIMS_SPECIES_DIR"] = fhi_aims_directory + species
+    species = "species_defaults/" + "defaults_" + str(defaults) + "/" + basis_set
+
+    """Set the relevant environment variables based on HPC"""
+    os.environ["AIMS_SPECIES_DIR"] = fhi_aims_directory[hpc] + species
+    if nodes_per_instance:
+        assert hpc in ["archer2", "hawk"], "Only ARCHER2 and Hawk supported for task-farming at the moment."
+        os.environ["ASE_AIMS_COMMAND"] = preamble[hpc] + task_farmed_commands[hpc] + fhi_aims_directory + executable
+    else:
+        os.environ["ASE_AIMS_COMMAND"] = preamble[hpc] + fhi_aims_directory + executable
