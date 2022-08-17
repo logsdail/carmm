ae431acf17c7f2d0fee901f3431d1019f64cd251
owainbeynon <54601295+owainbeynon@users.noreply.github.com>
2022-08-17 10:58:55 +0100

    Update __init__.py

--- carmm/cli/__init__.py
+++ carmm/cli/__init__.py
@@ -6,4 +6,34 @@ import click_completion
 
 
 from . import vib
+click_completion.init()
+
+
+@click.command(cls=AliasedGroup)
+@click.version_option(vibes_version, "-V", "--version")
+@click.option("-v", "--verbose", is_flag=True, hidden=True)
+@click.option("--silent", is_flag=True, help="set verbosity level to 0")
+@click.pass_context
+
+def cli(ctx, verbose, silent):
+    "carmm: "
+    if verbose:
+        verbosity = 2
+    elif silent:
+        verbosity = 0
+    else:
+        verbosity = 1
+
+    ctx.obj = CliTracker(verbose=verbosity)
+    ctx.help_option_names = ["-h", "--help"]
+
+    if verbosity > 1:
+        click.echo(f"Welcome to carmm!\n")
+
+
+cli.add_command(info.info)
+cli.add_command(analyse.analyse)
+cli.add_command(run.run)
+cli.add_command(build.build)
+
 
