8f74f39050a38dd772c79b0e700acb3875d1abf1
Jack Warren <Warrenje@cardiff.ac.uk>
2022-08-17 17:05:13 +0100

    Example for the colour bars, showing the format of the variables and viewing the figure

---
+++ examples/Build_colour_bars.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+def example_color_bar(width, height, bottom, vmin, vmax, orientation, label, cmapcolour):
+    """Basic Colour Bar
+    figsize= y axis how tall
+    bottom = <0.8 - size of bar (cannot be bigger than top)
+    vmin/vmax = x axis values
+    orientation = horizontal,vertical
+    label = units
+                                    """
+
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+    from matplotlib import cm
+
+    fig, ax = plt.subplots(figsize=(width, height))
+    fig.subplots_adjust(bottom=bottom)
+    colour = cm.get_cmap(cmapcolour, 1000)
+    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+
+    fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=colour),
+                cax=ax, orientation=orientation, label=label)
+
+    return fig
+
+
+fig = example_color_bar(1, 2, 0.5, -0.015, 0.015, 'horizontal', 'au', 'cool')
+plt.show()
