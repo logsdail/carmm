d1005fda4e9e55d0f8eb8e0121e2befc61d55661
Gabriel Bramley <BramleyG@cardiff.ac.uk>
2022-11-04 12:14:59 +0000

    Updated example to work with lone pair functionality.

--- examples/build_adsorbate_place_and_rotate.py
+++ examples/build_adsorbate_place_and_rotate.py
@@ -13,21 +13,21 @@ def test_adsorbate_placer():
     from ase import Atoms
 
     molecule = molecule('CH3CH2OH')
+
     h_atom = Atoms('H', positions=[(0, 0, 0)])
 
     site = read("data/H-Y_cluster/H-Y_cluster.xyz")
 
     zeolite, rotated_ads = place_adsorbate(h_atom, site, 0, 0, 1.0)
 
-    ads_and_site, rotated_ads = rotate_and_place_adsorbate(molecule, zeolite, 1.0,
-                                                           2, 0, 0,
+    ads_and_site, rotated_ads = rotate_and_place_adsorbate(molecule, zeolite, 1.5,
+                                                           2, -1, 0,
                                                            rotation=[-45, 0, -45])
 
-    comp_pos1 = np.array([2.00311656e+01, 5.31509397e+00, 1.89702619e+00])
-    comp_pos2 = np.array([2.04405930e+01, 4.53669268e+00, 2.54385921e+00])
-
+    comp_pos1 = np.array([1.96776122e+01,  5.66864736e+00,  1.89702619e+00])
+    comp_pos2 = np.array([1.90289830e+01,  5.04410070e+00,  6.82460837e-01])
     error_pos1 = np.linalg.norm(comp_pos1 - rotated_ads.positions[0], axis=-1)
-    error_pos2 = np.linalg.norm(comp_pos2 - rotated_ads.positions[-1], axis=-1)
+    error_pos2 = np.linalg.norm(comp_pos2 - rotated_ads.positions[1], axis=-1)
 
     assert np.isclose(error_pos1, 0, rtol=0, atol=1e-06), f"Error = {error_pos1}"
     assert np.isclose(error_pos2, 0, rtol=0, atol=1e-06), f"Error = {error_pos2}"
