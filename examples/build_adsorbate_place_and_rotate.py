fa0d65ee7571c5ed7282e64d4ab2f75b5e8517e6
Gabriel Bramley <BramleyG@cardiff.ac.uk>
2022-11-04 16:48:20 +0000

    - Updated test.

--- examples/build_adsorbate_place_and_rotate.py
+++ examples/build_adsorbate_place_and_rotate.py
@@ -13,10 +13,9 @@ def test_adsorbate_placer():
     from ase import Atoms
 
     mth = molecule('CH3CH2OH')
+    site = read("data/H-Y_cluster/H-Y_cluster.xyz")
 
     h_atom = Atoms('H', positions=[(0, 0, 0)])
-
-    site = read("data/H-Y_cluster/H-Y_cluster.xyz")
 
     h_placed = RotationBox(h_atom, site, 0, 0, 1.0, lps=2)
     h_placed.place_adsorbate()
@@ -24,17 +23,17 @@ def test_adsorbate_placer():
     mth_placed = RotationBox(mth, h_placed.ads_and_site, 2, -1, 1.5, lps=1)
     mth_placed.place_adsorbate()
 
-    mth_placed.rotate([-45,0,-45])
+    mth_placed.rotate([-45, 0, -45])
 
-    comp_pos1 = np.array([1.96776122e+01,  5.66864736e+00,  1.89702619e+00])
-    comp_pos2 = np.array([1.90289830e+01,  5.04410070e+00,  6.82460837e-01])
+    comp_pos1 = np.array([18.78360766, 5.44998722, -0.87248517])
+    comp_pos2 = np.array([20.89866577, 4.39477469, -0.93801803])
     error_pos1 = np.linalg.norm(comp_pos1 - mth_placed.atoms_ads.positions[0], axis=-1)
-    error_pos2 = np.linalg.norm(comp_pos2 - mth_placed.atoms_ads.positions[1], axis=-1)
+    error_pos2 = np.linalg.norm(comp_pos2 - mth_placed.atoms_ads.positions[2], axis=-1)
 
     assert np.isclose(error_pos1, 0, rtol=0, atol=1e-06), f"Error = {error_pos1}"
     assert np.isclose(error_pos2, 0, rtol=0, atol=1e-06), f"Error = {error_pos2}"
 
     from ase.visualize import view
-    view(ads_and_site)
+    view(mth_placed.ads_and_site)
 
 test_adsorbate_placer()
