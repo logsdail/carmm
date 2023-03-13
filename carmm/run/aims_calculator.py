fc45f32c05a1e0f88beca536f5a33a4dac5cceeb
robinsonmt1 <115092422+robinsonmt1@users.noreply.github.com>
2023-03-13 10:37:58 +0000

    Update aims_calculator.py
    Added a bit more detail and clarity to codata_warning to explain each process and why the conversions differ

--- carmm/run/aims_calculator.py
+++ carmm/run/aims_calculator.py
@@ -117,11 +117,13 @@ def get_aims_and_sockets_calculator(dimensions,
         print("You are using i-Pi based socket connectivity between ASE and FHI-aims.")
         print("The communicated energy in Hartree units will be converted to eV in ASE and not FHI-aims.")
         print("The eV/Hartree unit in FHI-aims is given by CODATA 2002 (Web Version 4.0 2003-12-09), Peter J. Mohr, Barry N. Taylor")
-        print("ASE uses CODATA 2014, thus the energy in eV from ASE and the FHI-aims outputs will differ.")
-        print("e.g. the same atoms.get_total_energy() from ASE will have been converted differently here than a non-sockets calculation")
-        print("Definition of the constants in each CODATA version can be found at https://wiki.fysik.dtu.dk/ase/_modules/ase/units.html in CODATA{} and create_units()")
+        print("ASE uses CODATA 2014, thus the output energy in eV from sockets (ASE conversion) and non-sockets (FHI-aims conversion) will differ.")
+        print("e.g. the same atoms.get_total_energy() from ASE will be different using the different calculators.")
+        print("Specifically, this is because in non-sockets Aims calculates in a.u., converts to eV (CODATA 2002) and passes that to ASE to output.")
+        print("But in sockets, Aims calculates in a.u., passes the a.u. value to ASE through this sockets calculator, ASE converts to eV (CODATA 2014) and outputs that.")
+        print("Definition of the constants in each CODATA version can be found at https://wiki.fysik.dtu.dk/ase/_modules/ase/units.html in CODATA{} and create_units().")
         print("PLEASE BE CONSISTENT IN THE UNIT CONVERSION FOR DATA ANALYSIS!")
-        print("If you have previous results from the non-sockets calculator, the energy conversion is approximately [Sockets] = [Non-sockets] * 1.00000005204439")
+        print("If you have previous results from the non-sockets calculator, the energy conversion is approximately [Sockets] = [Non-sockets] * 1.00000005204439.")
         print("You can turn off this message by setting 'codata_warning' keyword to False.")
 
     return socket_calc, fhi_calc
