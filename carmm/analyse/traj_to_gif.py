ab8625bef97a3342e49205857c018a6dcf7ea317
Matt Robinson <robinsonmt1@cardiff.ac.uk>
2023-06-06 16:34:05 +0100

    Updated atom_sub function

--- carmm/analyse/traj_to_gif.py
+++ carmm/analyse/traj_to_gif.py
@@ -38,8 +38,6 @@ def traj_to_gif(filename, frames_per_second=30, pause_time=0.5, atom_subs=None, 
 
 def atom_sub(atoms, atom_subs, steps, file, ext):
 
-    frame_atoms_list = []
-
     #Replace atoms of one element with another for clearer visualisation
     for frame in range(steps):
         frame_atoms = atoms[frame]
@@ -48,10 +46,9 @@ def atom_sub(atoms, atom_subs, steps, file, ext):
                 for i in range(len(frame_atoms.symbols)):
                     if frame_atoms.symbols[i] == list[0]:
                         frame_atoms.symbols[i] = list[1]
-        frame_atoms.append(frame_atoms_list)
         frame_atoms.write(f'{file}_povray.{ext}', append=True)
 
-    return frame_atoms_list
+    return frame_atoms
 
 def povray_render(atoms, steps, file, ext, atom_subs):
 
--- examples/analyse_traj_to_gif.py
+++ examples/analyse_traj_to_gif.py
@@ -7,10 +7,11 @@ def test_traj_to_gif():
     from carmm.analyse.traj_to_gif import traj_to_gif, gifmaker, atom_sub
     from ase.io import read
 
-    file = 'data/NH3-H3O_traj/nh3-h3o.traj'
-
     # Call the overall function like this, the rest of the script just tests the functions individually
-    # traj_to_gif(file, frames_per_second=10, pause_time=1, atom_subs=[['N', 'C']], keep_temp_files=True)
+    # traj_to_gif('data/NH3-H3O_traj/nh3-h3o.traj@:', frames_per_second=10,
+    #             pause_time=1, atom_subs=[['N', 'C']], keep_temp_files=True)
+
+    file = 'data/NH3-H3O_traj/nh3-h3o.traj@:'
 
     atoms = read(file)
     frame_atoms_list = atom_sub(atoms, [['N', 'C']], 41, 'nh3-h3o', 'traj')
