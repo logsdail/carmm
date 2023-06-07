031a76082d7cb9b3350a39949587e59c41093c81
Matt Robinson <robinsonmt1@cardiff.ac.uk>
2023-06-07 09:58:36 +0100

    Fixed atom_sub function call

--- carmm/analyse/traj_to_gif.py
+++ carmm/analyse/traj_to_gif.py
@@ -35,6 +35,9 @@ def traj_to_gif(filename, frames_per_second=30, pause_time=0.5, atom_subs=None, 
 
     print("Have a nice day!")
 
+    # For testing purposes
+    return file, ext, steps
+
 
 def atom_sub(atoms, atom_subs, steps, file, ext):
 
