a126bd3bb8387d0bd5a5597eca872610af442f54
Matt Robinson <robinsonmt1@cardiff.ac.uk>
2023-06-28 15:32:12 +0100

    Added second atom substitution to demonstrate 'list of list of strings' input type more clearly

--- examples/analyse_traj_to_gif.py
+++ examples/analyse_traj_to_gif.py
@@ -9,7 +9,7 @@ def test_traj_to_gif():
 
     # Call the overall function like this, the rest of the script just tests the functions individually
     # traj_to_gif('data/NH3-H3O_traj/nh3-h3o.traj', frames_per_second=10,
-    #             pause_time=1, atom_subs=[['N', 'C']], keep_temp_files=True)
+    #             pause_time=1, atom_subs=[['N', 'C'], ['O', 'N']], keep_temp_files=True)
 
     file = 'data/NH3-H3O_traj/nh3-h3o.traj'
     file, ext, steps = traj_to_gif(file, test=True)
@@ -18,9 +18,10 @@ def test_traj_to_gif():
     assert steps == 41
 
     atoms = read('data/NH3-H3O_traj/nh3-h3o.traj@:')
-    frame_atoms_list = atom_sub(atoms, [['N', 'C']], 41, 'nh3-h3o', 'traj')
+    frame_atoms_list = atom_sub(atoms, [['N', 'C'], ['O', 'N']], 41, 'nh3-h3o', 'traj')
     assert len(frame_atoms_list) == 41
     assert frame_atoms_list[11].symbols[4] == 'C'  # Random frame
+    assert frame_atoms_list[38].symbols[0] == 'N'  # Random frame
 
     fps = 10
     filenames, delay = gifmaker(41, 'nh3-h3o', 'traj', frames_per_second=fps, pause_time=1, keep_png_files=False, test=True)
