d9f8cec43f47a99b58f902d6f591baa03cb037ff
Matt Robinson <robinsonmt1@cardiff.ac.uk>
2023-07-04 15:54:49 +0100

    Renamed 'keep_png_files' to 'clean' for simplicity

--- carmm/analyse/traj_to_gif.py
+++ carmm/analyse/traj_to_gif.py
@@ -4,7 +4,7 @@ import os
 
 
 def traj_to_gif(filename, frames_per_second=30, pause_time=0.5, atom_subs=None, convert_flags=None,
-                keep_temp_files=True, test=False):
+                clean=True, test=False):
     """
     A function which takes a .traj file, visualises it in povray with your desired settings and outputs a .gif file.
     When the function gives you the ase viewer, rotate to your desired view, go to Tools -> Render Scene, select the
@@ -21,7 +21,7 @@ def traj_to_gif(filename, frames_per_second=30, pause_time=0.5, atom_subs=None, 
     all images for clearer visualisation
     :param convert_flags: (dict of strings) Flags and corresponding parameters for the ImageMagick 'convert' function.
     For default parameters, leave out this parameter. Default options are '-verbose -dispose previous -loop 0'
-    :param keep_temp_files: (boolean) If set to False, will delete the created povray .traj and .png files after use
+    :param clean: (boolean) If set to False, will delete the created povray .traj and .png files after use
     :param test: (boolean) Do not change! A quick fix to stop the unittest from getting stuck waiting for a user input
     :return: A .gif file of the .traj file, visualised in povray
     """
@@ -38,7 +38,7 @@ def traj_to_gif(filename, frames_per_second=30, pause_time=0.5, atom_subs=None, 
     if not test:
         povray_render(atoms, steps, file, ext, atom_subs, test)
 
-    gifmaker(steps, file, ext, frames_per_second, pause_time, convert_flags, keep_temp_files, test)
+    gifmaker(steps, file, ext, frames_per_second, pause_time, convert_flags, clean, test)
 
     print("Happy cooking!")
 
@@ -84,7 +84,7 @@ def povray_render(atoms, atom_subs, steps, file, ext, test):
         input('***Press Enter to continue once Povray is finished visualising...***\n')
 
 
-def gifmaker(steps, file, ext, frames_per_second, pause_time, convert_flags, keep_png_files, test):
+def gifmaker(steps, file, ext, frames_per_second, pause_time, convert_flags, clean, test):
     # Generate the list of povray image filenames
     digits = len(str(steps - 1))
     indices = [f'%d0{digits}' % i for i in range(steps)]
@@ -120,7 +120,7 @@ def gifmaker(steps, file, ext, frames_per_second, pause_time, convert_flags, kee
         os.system(command)
 
         # Delete the povray image files if requested
-        if not keep_png_files:
+        if not clean:
             for index in indices:
                 os.system(f'rm {file}.{index}.png')
             os.system(f'rm {file}_povray.{ext}')
