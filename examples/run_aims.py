942c8f6efa14a82ae728c71804b9d8bcd43837aa
Igor Kowalec <58031188+ikowalec@users.noreply.github.com>
2023-09-12 12:00:57 +0100

    Adjust the AIMS_COMMAND to allow task farming on HAWK

--- examples/run_aims.py
+++ examples/run_aims.py
@@ -12,30 +12,38 @@ def test_run_aims():
     import os
 
     expected_paths = {
-        'hawk': 'time mpirun -np $SLURM_NTASKS /apps/local/projects/scw1057/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
-        'hawk-amd': 'time mpirun -np $SLURM_NTASKS /apps/local/projects/scw1057/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
+        'hawk': 'time srun --nodes=$SLURM_NNODES --ntasks=$SLURM_NTASKS -d mpirun /apps/local/projects/scw1057/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
+        'hawk-amd': 'time srun --nodes=$SLURM_NNODES --ntasks=$SLURM_NTASKS -d mpirun /apps/local/projects/scw1057/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
         'isambard': 'time aprun -n $NPROCS /home/ca-alogsdail/fhi-aims-gnu/bin/aims.$VERSION.scalapack.mpi.x',
-        'archer2': 'srun --cpu-bind=cores --distribution=block:block --hint=nomultithread /work/e05/e05-files-log/shared/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
-        'young': 'gerun /home/mmm0170/Software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
-        'aws': 'time srun --mpi=pmi2 --hint=nomultithread --distribution=block:block apptainer exec /shared/logsdail_group/sing/mkl_aims_2.sif bash /shared/logsdail_group/sing/sing_fhiaims_script.sh $@'
+        'archer2': 'srun --cpu-bind=cores --distribution=block:block --hint=nomultithread  /work/e05/e05-files-log/shared/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
+        'young': 'gerun  /home/mmm0170/Software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
+        'aws': 'time srun --mpi=pmi2 --hint=nomultithread --distribution=block:block  apptainer exec /shared/logsdail_group/sing/mkl_aims_2.sif bash /shared/logsdail_group/sing/sing_fhiaims_script.sh $@'
     }
 
     expected_paths_taskfarm = {
-        'hawk': "time mpirun -np $SLURM_NTASKS --nodes=1 --ntasks=40 /apps/local/projects/scw1057/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x",
-        'hawk-amd': 'time mpirun -np $SLURM_NTASKS --nodes=1 --ntasks=64 /apps/local/projects/scw1057/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
+        'hawk': "time srun --nodes=1 --ntasks=40 -d mpirun /apps/local/projects/scw1057/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x",
+        'hawk-amd': 'time srun --nodes=1 --ntasks=64 -d mpirun /apps/local/projects/scw1057/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
         'isambard': '',
         'archer2': 'srun --cpu-bind=cores --distribution=block:block --hint=nomultithread --nodes=1 --ntasks=128 /work/e05/e05-files-log/shared/software/fhi-aims/bin/aims.$VERSION.scalapack.mpi.x',
         'young': '',
         'aws': 'time srun --mpi=pmi2 --hint=nomultithread --distribution=block:block --nodes=1 --ntasks=72 apptainer exec /shared/logsdail_group/sing/mkl_aims_2.sif bash /shared/logsdail_group/sing/sing_fhiaims_script.sh $@'
     }
 
+
+
     for hpc in ['hawk', 'hawk-amd', 'isambard', 'archer2', 'young', 'aws']:
+        '''Assign the executable command based on HPC'''
         set_aims_command(hpc)
-        assert os.environ['ASE_AIMS_COMMAND'] == expected_paths[hpc]
+
+
+
+        assert os.environ['ASE_AIMS_COMMAND'] == expected_paths[hpc], f"Path incorrect on {hpc}: {expected_paths[hpc]}\n" \
+                                                                      f"Currently: {os.environ['ASE_AIMS_COMMAND']}"
 
         if hpc in ["hawk", 'hawk-amd', 'archer2', 'aws']:
             set_aims_command(hpc, nodes_per_instance=1)
-            assert os.environ['ASE_AIMS_COMMAND'] == expected_paths_taskfarm[hpc]
+            assert os.environ['ASE_AIMS_COMMAND'] == expected_paths_taskfarm[hpc], f"Path incorrect on {hpc}: {expected_paths_taskfarm[hpc]}\n" \
+                                                                                   f"Currently: {os.environ['ASE_AIMS_COMMAND']}"
 
     import ase # Necessary to check the code version, as socket functionality has changed
     ase_major_version = int(ase.__version__.split(".")[0])
