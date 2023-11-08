6a85341b179be8c8f158ff875b966b1caf60e3f9
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2023-11-08 11:28:27 +0000

    Update aims_path.py
    Tidying and restructure to better distinguish task-farming setup of calculations. Gives foundations for future work to update process with less hard-coded variables.

--- carmm/run/aims_path.py
+++ carmm/run/aims_path.py
@@ -57,40 +57,56 @@ def set_aims_command(hpc='hawk', basis_set='light', defaults=2010, nodes_per_ins
     """Set the relevant environment variables based on HPC"""
     os.environ["AIMS_SPECIES_DIR"] = fhi_aims_directory[hpc] + species
 
-    no_cpus = {"hawk": 40,
-              "hawk-amd": 64,
-              "archer2": 128,
-              "isambard": 64,
-              "young":64,
-              "aws": 72}
+    cpus_per_node = {
+        "hawk": 40,
+        "hawk-amd": 64,
+        "archer2": 128,
+        "isambard": 64,
+        "young":64,
+        "aws": 72
+    }
 
+    if hpc in ["hawk","hawk-amd"]:
+        requested_tasks = os.environ["SLURM_NTASKS"]
+        requested_nodes = os.environ["SLURM_NNODES"]
 
+        # Check if using a full node, for efficiency
+        if (requested_tasks/requested_nodes) % cpus_per_node[hpc] not 0:
+            print("WARNING: You are not using all the CPUs on the requested nodes.")
+            print("         Check if you are accidentally underpopulating the nodes.")       
+
+    cpu_command = {
+        "hawk": f"--nodes=$SLURM_NNODES --ntasks=$SLURM_NTASKS -d mpirun",
+        "hawk-amd": f"--nodes=$SLURM_NNODES --ntasks=$SLURM_NTASKS -d mpirun",
+        "isambard": "-n $NPROCS",
+        "archer2": "",
+        "young": "",
+        "aws": ""
+    }
+    
     if nodes_per_instance:
-        #TODO: change name to CPU count and make a separate dictionary for non taskfarmed
-        cpu_count_setting = {
-            "archer2": f"--nodes={nodes_per_instance} --ntasks={int(no_cpus[hpc] * nodes_per_instance)}",
-            "hawk": f"--nodes={nodes_per_instance} --ntasks={int(no_cpus[hpc] * nodes_per_instance)} -d mpirun",
-            "hawk-amd": f"--nodes={nodes_per_instance} --ntasks={int(no_cpus[hpc] * nodes_per_instance)} -d mpirun",
-            "aws": f"--nodes={nodes_per_instance} --ntasks={int(no_cpus[hpc] * nodes_per_instance)}",
-            # TODO: add and test isambard and young task-farmed commands
-            "isambard": "",
-            "young": "",
-        }
+
+        # Moved assertions up so we check validity before proceeding.
+        # Todo: Add Isambard/Young as needed
+        assert hpc in ["archer2", "hawk", "hawk-amd", "aws"], \
+            "Only ARCHER2, Hawk and AWS supported for task-farming at the moment."
 
         if hpc == "aws":
             assert nodes_per_instance == 1, "FHI-aims does not run on more than one node on AWS at present."
 
-        assert hpc in ["archer2", "hawk", "hawk-amd", "aws"], \
-            "Only ARCHER2, Hawk and AWS supported for task-farming at the moment."
-
-    else:
-        cpu_count_setting = {
-            "hawk": f"--nodes=$SLURM_NNODES --ntasks=$SLURM_NTASKS -d mpirun",
-            "hawk-amd": f"--nodes=$SLURM_NNODES --ntasks=$SLURM_NTASKS -d mpirun",
-            "isambard": "-n $NPROCS",
-            "archer2": "",
-            "young": "",
-            "aws": ""
+        # Can only be initialised if nodes_per_instance exists
+        # Todo: Can we get total nodes from all runtime environments, and then use same
+        #       one dictionary for task-farming and normal single run (i.e. nodes_per_instance == total_nodes)
+        cpu_command_task_farming = {
+            "archer2": f"--nodes={nodes_per_instance} --ntasks={int(cpus_per_node[hpc] * nodes_per_instance)}",
+            "hawk": f"--nodes={nodes_per_instance} --ntasks={int(cpus_per_node[hpc] * nodes_per_instance)} -d mpirun",
+            "hawk-amd": f"--nodes={nodes_per_instance} --ntasks={int(cpus_per_node[hpc] * nodes_per_instance)} -d mpirun",
+            "aws": f"--nodes={nodes_per_instance} --ntasks={int(cpus_per_node[hpc] * nodes_per_instance)}",
         }
 
-    os.environ["ASE_AIMS_COMMAND"] = f"{preamble[hpc]} {cpu_count_setting[hpc]} {executable}"
+    if not nodes_per_instance:
+        # Default is setup a standard single run calculation
+        os.environ["ASE_AIMS_COMMAND"] = f"{preamble[hpc]} {cpu_command[hpc]} {executable}"
+    else:
+        # Setup runtime for task-farming. 
+        os.environ["ASE_AIMS_COMMAND"] = f"{preamble[hpc]} {cpu_command_task_farming[hpc]} {executable}"
