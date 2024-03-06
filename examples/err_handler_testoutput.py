0461997c5539f010724c63a12b55b855ea59f285
Gabriel Bramley <BramleyG@cardiff.ac.uk>
2024-03-06 14:04:31 +0000

    Added rudimentary error handling with logging

---
+++ examples/err_handler_testoutput.py
@@ -0,0 +1,11 @@
+
+def test_set_logger():
+    from carmm.err_handler.logger_set import set_logger
+
+    #TODO: MAKE THIS TEST ACTUALLY TEST CAPTURED OUTPUT
+
+    logger = set_logger("test_logger", 0)
+
+    logger.debug('test_set_logger test passed! DEBUG error correctly printed')
+
+test_set_logger()
