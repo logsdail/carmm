Fixed default Morse potential for more reliable rotation paths and updated unit test.

carmm/build/neb/geodesic.py
@@ -90,7 +90,7 @@ class GeodesicInterpolator:
 
         bonds_im1 = self.get_atoms_bonds(image1)
         bonds_im2 = self.get_atoms_bonds(image2)
-        morse = Morse(alpha=0.7, re=1.5, beta=0.001)
+        morse = Morse(alpha=1.6, re=1.5, beta=0.001)
         friction = 0.1 / np.sqrt(len(image1))
 
         comb_bonds = list(set(bonds_im1) | set(bonds_im2))

examples/build_neb_geodesic.py
@@ -1,7 +1,8 @@
 def test_geodesic_interpolator():
     from carmm.build.neb.geodesic import GeodesicInterpolator
-    from ase.io import read
+    from ase.io import read, write
     from ase.build import molecule
+    from ase.neb import idpp_interpolate
     import numpy as np
 
     initial = molecule('C2H6')
@@ -13,11 +14,13 @@ def test_geodesic_interpolator():
     Geodesic = GeodesicInterpolator(initial, final, 6)
     Geodesic.init_path()
 
+    idpp_interpolate(Geodesic.images)
+
     ref_images = read("./data/C2H6_path/path.traj", index=":")
     rmsd = [ Geodesic.cart_rmsd(atoms1=ref_image, atoms2=test_im) \
                             for ref_image, test_im in zip(ref_images, Geodesic.images)]
 
-    assert np.any(np.array(rmsd) > 0.01), "Geodesic test failed - difference in geometry greater than 0.01 compared to reference."
+    assert np.all(np.array(rmsd) < 0.01), "Geodesic test failed - difference in geometry greater than 0.01 compared to reference."
 
 test_geodesic_interpolator()
 