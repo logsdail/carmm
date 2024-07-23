Improved geodesic interpolator, correcting gradient errors and with vastly improved runtimes.

carmm/build/neb/geodesic_utils.py
@@ -1,22 +1,79 @@
 from ase.geometry import get_distances, get_distances_derivatives
 import numpy as np
-def get_bond_dist_and_deriv(atoms, bond_list, input_pos=None, deriv_only=False):
+from functools import wraps
+from time import time
+
+def timing(f):
+    """
+
+    Parameters
+    ----------
+    f
+
+    Returns
+    -------
+
+    """
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print('func:%r took: %2.4f sec' % \
+          (f.__name__, te-ts))
+        return result
+    return wrap
+
+def get_bond_dist_and_deriv(atoms, bond_list, input_pos=None, deriv_only=False, constraints=None):
+    """
+    Calculates distances between bonds in bond list and the derivatives of corresponding bond vectors
+    with respect to cartesian coordinates of all atoms
+
+    Parameters
+    ----------
+    atoms : ASE Atoms Object
+        Atom information in ASE format
+    bond_list : list
+        List of tuples containing all considered bonds
+    input_pos : ndarray
+        1D array of positions, replacing positions of atoms (dim: len(atoms)*3)
+    deriv_only : bool
+        Returns only derivatives
+    constraints : list
+        List of fixed atomic positions
+
+    Returns
+    -------
+    distances_arr : ndarray
+        Array of distances between atoms specified in bond_list (dim: len(bond_list))
+    derivatives_arr : ndarray
+        Array of derivatives of bond vectors by cartesian coordinates of all atoms (dim: [len(bond_list), len(atoms), 3])
+    """
 
     if input_pos is not None:
+        save_positions = atoms.positions.copy()
         atoms.positions = input_pos.reshape(-1, 3)
 
-    distances_arr = np.zeros(shape=(len(atoms), len(atoms)))
-    derivatives_arr = np.zeros(shape=(len(atoms), len(atoms), 3))
+    bond_array = np.array(bond_list)
+    distances_arr = np.zeros(shape=(len(bond_list)))
+    derivatives_arr = np.zeros(shape=(len(bond_list), len(atoms), 3))
 
-    for bond in bond_list:
-        idx1, idx2 = bond[0], bond[1]
+    distances, vects = atoms.get_all_distances(mic=True), atoms.get_all_distances(mic=True, vector=True)
+    distances_arr = np.array([ distances[idx1, idx2] for idx1, idx2 in zip(bond_array[:,0], bond_array[:,1]) ])
 
-        dist = atoms.get_distance(idx1, idx2, mic=True)
-        dist_vect = atoms.get_distance(idx1, idx2, mic=True, vector=True)
-        deriv = get_distances_derivatives([dist_vect], cell=atoms.cell, pbc=atoms.pbc)[0][0][0]
+    bond_vects = np.array([ vects[idx1, idx2, :] for idx1, idx2 in zip(bond_array[:,0], bond_array[:,1]) ])
+    derivatives = get_distances_derivatives(bond_vects, cell=atoms.get_cell(), pbc=atoms.pbc)
 
-        distances_arr[idx1, idx2] = dist
-        derivatives_arr[idx1, idx2] = deriv
+    for idx, bond in enumerate(bond_list):
+        derivatives_arr[idx, bond[0], :] = derivatives[idx, 0, :]
+        derivatives_arr[idx, bond[1], :] = derivatives[idx, 1, :]
+
+    if constraints is not None:
+        derivatives_arr[:, constraints, :] = np.array([0, 0, 0])
+        derivatives_arr[:, constraints, :] = np.array([0, 0, 0])
+
+    if input_pos is not None:
+        atoms.positions = save_positions
 
     if deriv_only:
         return derivatives_arr
@@ -24,75 +81,165 @@ def get_bond_dist_and_deriv(atoms, bond_list, input_pos=None, deriv_only=False):
     return distances_arr, derivatives_arr
 
 
-def get_scaled_bond_dist_and_deriv(atoms, bond_list, morse=None, input_pos=None):
-    distance_arr, derivative_arr = get_bond_dist_and_deriv(atoms, bond_list, input_pos=input_pos)
+def get_scaled_bond_dist_and_deriv(atoms, bond_list, morse=None, input_pos=None, constraints=None):
+    """
+    Calculates distances between bonds in bond list and the derivatives of corresponding bond vectors
+    with respect to cartesian coordinates of all atoms, scaled by an approximate Morse potential.
+
+    Parameters
+    ----------
+    atoms : ASE Atoms Object
+        Atom information in ASE format
+    bond_list : list
+        List of tuples containing all considered bonds
+    morse : Class Morse
+        Morse potential, which scales both distances and derivatives depending on pairwise distance and species
+    input_pos : ndarray
+        1D array of positions, replacing positions of atoms (dim: len(atoms)*3)
+    constraints : list
+        List of fixed atomic positions
+
+    Returns
+    -------
+    distances_arr : ndarray
+        Array of distances between atoms specified in bond_list, scaled by Morse potential (dim: len(bond_list))
+    derivatives_arr : ndarray
+        Array of derivatives of bond vectors by cartesian coordinates of all atoms, scaled by Morse potential
+        (dim: [len(bond_list) * len(atoms), 3]).
+    """
+
+    distance_arr, derivative_arr = get_bond_dist_and_deriv(atoms, bond_list, input_pos=input_pos, constraints=constraints)
 
     if morse is None:
         # If not specified, just use default Morse parameters
         morse = Morse()
 
-    dist_func = np.frompyfunc(morse.potential_en, 1, 1)
-    deriv_func = np.frompyfunc(morse.potential_der, 1, 1)
+    morse.get_re(atoms, bond_list)
 
-    scaled_distance_arr = dist_func(distance_arr, where=distance_arr > 0, out=np.zeros(distance_arr.shape),
-                                    casting='unsafe')
+    dist_func = np.frompyfunc(morse.potential_en, 2, 1)
+    deriv_func = np.frompyfunc(morse.potential_der, 2, 1)
 
-    scale_deriv = deriv_func(distance_arr, where=distance_arr > 0, out=np.zeros(distance_arr.shape), casting='unsafe')
-    scaled_derivative_arr = np.einsum('ijk, ij -> ijk', derivative_arr, scale_deriv)
+    #scaled_distance_arr = dist_func(distance_arr, morse.re_list, where=distance_arr > 0, out=np.zeros(distance_arr.shape),
+    #                                casting='unsafe')
+    scaled_distance_arr = dist_func(distance_arr, morse.re_list, out=np.zeros(distance_arr.shape), casting='unsafe')
+    #scale_deriv = deriv_func(distance_arr, morse.re_list, where=distance_arr > 0, out=np.zeros(distance_arr.shape), casting='unsafe')
+    scale_deriv = deriv_func(distance_arr, morse.re_list, out=np.zeros(distance_arr.shape), casting='unsafe')
 
-    return scaled_distance_arr, scaled_derivative_arr
+    scaled_derivative_arr = np.einsum('ijk, i -> ijk', derivative_arr, scale_deriv)
+    #for idx, grad in enumerate(scale_deriv):
+    #    derivative_arr[idx] *= grad
 
+    return scaled_distance_arr, scaled_derivative_arr.reshape(len(bond_list), -1)
 
 class Morse:
-    def __init__(self, re=1.5, alpha=0.7, beta=0.01):
-        self.re = re
+    def __init__(self, alpha=0.7, beta=0.01):
         self.alpha = alpha
         self.beta = beta
 
-    def potential_en(self, dist):
-        ratio = dist / self.re
+    def potential_en(self, dist, re):
+        ratio = dist / re
         val1 = np.exp(self.alpha * (1 - ratio))
         val2 = self.beta / ratio
 
         return val1 + val2
 
-    def potential_der(self, dist):
-        ratio = dist / self.re
+    def potential_der(self, dist, re):
+        ratio = dist / re
         val1 = np.exp(self.alpha * (1 - ratio))
         val2 = self.beta / ratio
-        deriv_scale = -self.alpha / self.re * val1 - (val2 / dist)
+        deriv_scale = -(val1 * self.alpha / re) - (val2 / dist)
 
         return deriv_scale
 
+    def get_re(self, atoms, bond_list):
 
-def cost_func(flat_positions, atoms, bond_list, av_wij, friction, morse):
-    wij, dwij = get_scaled_bond_dist_and_deriv(atoms, bond_list, input_pos=flat_positions, morse=morse)
-    wij0, dwij0 = get_scaled_bond_dist_and_deriv(atoms, bond_list, morse=morse)
-    wij = wij - av_wij
+        from ase.data import covalent_radii
 
-    # Return as relevant bond list - otherwise least-squares problem blows up to natomsxnatomsxnatomsx3
-    # for the Jacobian
-    output_wij = np.zeros(len(bond_list))
+        self.re_list = []
+        for bond in bond_list:
+            bond_idx1, bond_idx2 = bond[0], bond[1]
+            atomic_number_1, atomic_number_2 = atoms.numbers[bond_idx1], atoms.numbers[bond_idx2]
 
-    for idx, (i, j) in enumerate(bond_list):
-        output_wij[idx] = wij[i, j]
+            self.re_list.append(covalent_radii[atomic_number_1] + covalent_radii[atomic_number_2])
 
-    output_wij = np.concatenate([output_wij, (flat_positions - atoms.get_positions().ravel()) * friction])
+        self.re_list = np.array(self.re_list)
+
+def cost_func(flat_positions, atoms, bond_list, av_wij, friction, morse, constraints):
+    """
+    Calculatos cost-function for minimisation of geodesic distance for newly generated image
+    between two images
+
+    Parameters
+    ----------
+    flat_positions : ndarray
+        1D array of positions, replacing positions of atoms (dim: len(atoms)*3)
+    atoms : ASE Atoms Object
+        Atom information in ASE format
+    bond_list : list
+        List of tuples containing all considered bonds
+    av_wij : ndarray
+        Averaged internal coordinate distance between the preceding and succeeding image of the optimised
+        image
+    friction : float64
+        Scaling factor
+    morse : Class Morse
+        Morse potential, which scales both distances and derivatives depending on pairwise distance and species
+    constraints : list
+        List of fixed atomic positions
+
+    Returns
+    -------
+    output_wij:
+        Cost function for least squares problem (dim: len(bond_list)+len(atoms))
+    """
+
+    wij, dwij = get_scaled_bond_dist_and_deriv(atoms, bond_list, input_pos=flat_positions, morse=morse, constraints=constraints)
+    wij = (wij - av_wij)
+
+    output_wij = np.concatenate([wij, (flat_positions - (atoms.get_positions().ravel())) * friction])
 
     return output_wij
 
+def cost_func_der(flat_positions, atoms, bond_list, av_wij, friction, morse, constraints):
+    """
+    Calculatos gradient of cost-function for minimisation of geodesic distance for newly generated image
+    between two images
 
-def cost_func_der(flat_positions, atoms, bond_list, av_wij, friction, morse):
-    wij, dwij = get_scaled_bond_dist_and_deriv(atoms, bond_list, input_pos=flat_positions, morse=morse)
+    Parameters
+    ----------
+    flat_positions : ndarray
+        1D array of positions, replacing positions of atoms (dim: len(atoms)*3)
+    atoms : ASE Atoms Object
+        Atom information in ASE format
+    bond_list : list
+        List of tuples containing all considered bonds
+    av_wij : ndarray
+        Averaged internal coordinate distance between the preceding and succeeding image of the optimised
+        image
+    friction : float64
+        Scaling factor
+    morse : Class Morse
+        Morse potential, which scales both distances and derivatives depending on pairwise distance and species
+    constraints : list
+        List of fixed atomic positions
 
+    Returns
+    -------
+    np.vstack([dwij, friction_scale]) : ndarray
+        Cost function gradient for least squares problem (dim:  [len(bond_list) * len(atoms) + len(atoms), 3])
+    """
+
+    wij, dwij = get_scaled_bond_dist_and_deriv(atoms, bond_list, input_pos=flat_positions, morse=morse, constraints=constraints)
     # Return as relevant bond list - otherwise least-squares problem blows up to natomsxnatomsxnatomsx3
     # for the Jacobian
-    output_dwij = np.zeros((len(bond_list), len(atoms), 3))
+    #output_dwij = np.zeros((len(bond_list), len(atoms), 3))
 
-    for idx, (i, j) in enumerate(bond_list):
-        output_dwij[idx, i] = dwij[i, j]
-        output_dwij[idx, j] = -dwij[i, j]
+    #for idx, (i, j) in enumerate(bond_list):
+    #    output_dwij[idx, i] = dwij[i, j]
+    #    output_dwij[idx, j] = -dwij[i, j]
 
     friction_scale = np.identity(flat_positions.size) * friction
 
-    return np.vstack([output_dwij.reshape((len(bond_list), 3 * len(atoms))), friction_scale])
+    return np.vstack([dwij, friction_scale])
+    #return np.vstack([dwij.reshape((len(bond_list), 3 * len(atoms))), friction_scale])
+