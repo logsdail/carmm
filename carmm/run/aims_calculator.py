9c1ba715362d7f2e98b52af838b4569fe1a33709
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2024-07-31 15:27:41 +0100

    Minor testing that may be ASE version agnostic solution for calculators

--- carmm/run/aims_calculator.py
+++ carmm/run/aims_calculator.py
@@ -129,19 +129,22 @@ def get_aims_and_sockets_calculator(dimensions,
     # **kwargs is a passthrough of keyword arguments
     fhi_calc = get_aims_calculator(dimensions, **kwargs)
     # Add in PIMD command to get sockets working
-    from carmm.utils.python_env_check import ase_env_check
-    if ase_env_check('3.23.0'):
-    # The set() doesn't work as of ASE v3.23, so instead here we create a new calculator with settings copied across
-    # and we add in the sockets flag. This is a bit of a hack but it works.
-        from ase.calculators.aims import Aims
-        fhi_calc = Aims(
-            template=fhi_calc.template,
-            profile=fhi_calc.profile,
-            directory=fhi_calc.directory,
-            parameters=fhi_calc.parameters,
-            use_pimd_wrapper=[host, port])
-    else: # Old method
-        fhi_calc.set(use_pimd_wrapper=[host, port])
+    #from carmm.utils.python_env_check import ase_env_check
+    #if ase_env_check('3.23.0'):
+    ## The set() doesn't work as of ASE v3.23, so instead here we create a new calculator with settings copied across
+    ## and we add in the sockets flag. This is a bit of a hack but it works.
+    #    from ase.calculators.aims import Aims
+    #    fhi_calc = Aims(
+    #        template=fhi_calc.template,
+    #        profile=fhi_calc.profile,
+    #        directory=fhi_calc.directory,
+    #        parameters=fhi_calc.parameters,
+    #        use_pimd_wrapper=[host, port])
+    #else: # Old method
+    #    fhi_calc.set(use_pimd_wrapper=[host, port])
+
+    # Turns out the above isn't needed if this works. Thanks @ikowalec
+    fhi_calc.parameters['use_pimd_wrapper']=[host, port]
 
     # Setup sockets calculator that "wraps" FHI-aims
     from ase.calculators.socketio import SocketIOCalculator
