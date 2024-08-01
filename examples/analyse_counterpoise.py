a2f77ec58f7b394f29b6179022cf3fe26778099f
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2024-08-01 12:58:48 +0100

    Simplified the counterpoise correction CI test, as we shouldn't need to change calculator type here depending on ASE version

--- examples/analyse_counterpoise.py
+++ examples/analyse_counterpoise.py
@@ -21,26 +21,34 @@ def test_analyse_counterpoise():
     CO = read('data/CO_BSSE/C_monoxide_pbe.traj')
     examples_directory = getcwd()
 
-    # Old calculator:
-    # Construct the calculator
+#    # This is now all managed inside get_aims_calculator, so not needed here
+#    # Keeping in case needed for legacy purposes
+#    # Old calculator:
+#    # Construct the calculator
+#
+#    if not ase_env_check('3.23.0'):
+#        toy_calc = get_aims_calculator(dimensions=0, xc='pbe', default_initial_moment=0.5,
+#                                       spin='collinear'
+#                                       directory=examples_directory+'/data/CO_BSSE',
+#                                       species_dir=examples_directory+'/data/CO_BSSE')
+#        toy_calc.set(spin='collinear', relativistic='atomic_zora scalar')
+#    else:
+#        from ase.calculators.aims import AimsProfile, Aims
+#        fake_profile = AimsProfile(command='', default_species_directory=examples_directory + '/data/CO_BSSE')
+#        toy_calc = get_aims_calculator(dimensions=0, xc='pbe', spin='collinear',
+#                                       default_initial_moment=0.5,
+#                                       directory=examples_directory+'/data/CO_BSSE',
+#                                       profile=fake_profile,
+#                                       species_dir=examples_directory+'/data/CO_BSSE')
+#        # Reverted this so we still use the get_aims_calculator - means we use one process only for definition
+#        #toy_calc = Aims(xc='pbe', spin='collinear', default_initial_moment=0.5,
+#        #                               relativistic='atomic_zora scalar', directory=examples_directory+'/data/CO_BSSE',
+#        #                               species_dir=examples_directory+'/data/CO_BSSE', profile=fake_profile)
 
-    if not ase_env_check('3.23.0'):
-        toy_calc = get_aims_calculator(dimensions=0, xc='pbe', default_initial_moment=0.5,
-                                       directory=examples_directory+'/data/CO_BSSE',
-                                       species_dir=examples_directory+'/data/CO_BSSE')
-        toy_calc.set(spin='collinear', relativistic='atomic_zora scalar')
-    else:
-        from ase.calculators.aims import AimsProfile, Aims
-        fake_profile = AimsProfile(command='', default_species_directory=examples_directory + '/data/CO_BSSE')
-        toy_calc = get_aims_calculator(dimensions=0, xc='pbe', spin='collinear',
-                                       default_initial_moment=0.5,
-                                       directory=examples_directory+'/data/CO_BSSE',
-                                       profile=fake_profile,
-                                       species_dir=examples_directory+'/data/CO_BSSE')
-        # Reverted this so we still use the get_aims_calculator - means we use one process only for definition
-        #toy_calc = Aims(xc='pbe', spin='collinear', default_initial_moment=0.5,
-        #                               relativistic='atomic_zora scalar', directory=examples_directory+'/data/CO_BSSE',
-        #                               species_dir=examples_directory+'/data/CO_BSSE', profile=fake_profile)
+    toy_calc = get_aims_calculator(dimensions=0, xc='pbe', default_initial_moment=0.5,
+                                   spin='collinear'
+                                   directory=examples_directory+'/data/CO_BSSE',
+                                   species_dir=examples_directory+'/data/CO_BSSE')
 
     # This function can work with lists of indices or symbols of the two parts in a binding complex for CP correction.
     # This does not work with socket calculator for now.
