8bd3089f2387c691d4cc8d56a5d0f45dfdeb2d4e
Andrew Logsdail <LogsdailA@cardiff.ac.uk>
2024-08-01 11:26:02 +0100

    Commenting in new functions

--- carmm/utils/python_env_check.py
+++ carmm/utils/python_env_check.py
@@ -3,21 +3,43 @@ Created on Fri 12/07/24
 
 @author Firas Assa
 
-deprecation() is a sub function of python_env_check() that outputs a DeprecationWarning
-
-python_env_check() function has an optional argument version, which when is entered outputs True boolean value if python
-interpreter version is >= current version. When no argument is entered, it checks whether python version > 3.7.
-
 is_env_python() function checks if the environment version entered as an argument is true, by printing 1 or 0
 """
 
-def deprecation(message):
+def deprecation(message, stacklevel=2):
+    """
+    A general purpose deprecation warning for use when raising future error warnings
+
+    Args:
+        message: str
+            Text to display with warning
+        stacklevel: int
+            Sets the number of lines to be unwound when raising the warning.
+
+    Returns:
+        Nothing
+            
+
+    """
+
     import warnings
 
     # sub-function of python_env_check()
-    warnings.warn(message, DeprecationWarning, stacklevel=2)
+    warnings.warn(message, DeprecationWarning, stacklevel=stacklevel)
 
 def ase_env_check(version=None):
+    """
+    A general purpose check for the ASE version being used, to filter code routes
+
+    Args:
+        version: str
+            Text representation of the version number for ASE
+
+    Returns:
+        Boolean: true if version is compatible, false if not
+
+    """
+
     import ase
 
     if version is None:
@@ -30,6 +52,17 @@ def ase_env_check(version=None):
         return False
 
 def python_env_check(minorversion=None):
+    """
+    A general purpose check for the Python minor version being used, to filter code routes
+
+    Args:
+        minorversion: int
+            Minor version number of Python to check. Currently assumes major version is 3.
+
+    Returns:
+        Boolean: true if version is compatible, false if not
+
+    """
     import sys
 
     if minorversion is None:
@@ -41,31 +74,66 @@ def python_env_check(minorversion=None):
         deprecation("Warning! Your current Python Interpreter is: " + sys.version + ", and will be deprecated soon.")
         return False
 
-def is_env_python_minor(version):
+def _is_env_python_minor(version):
+    """
+    A sub-function of is_env_python, checking minor version exact match
+
+    Args:
+        version: int
+            Minor version number of Python to check. Currently assumes major version is 3.
+
+    Returns:
+        Boolean: true if version is identical, false if not
+
+    """
+
     import sys
 
-    #sub-function of is_env_python()
     if sys.version_info.minor == version:
         return True
     else:
         return False
 
-def is_env_python_major_and_minor(version):
+def _is_env_python_major_and_minor(version):
+    """
+    A sub-function of is_env_python, checking major and minor version exact match
+
+    Args:
+        version: tuple, int
+            Major and minor version number of Python to check.
+
+    Returns:
+        Boolean: true if version is identical, false if not
+
+    """
+
+
     import sys
 
-    # sub-function of is_env_python()
     if sys.version_info.major == version[0] and sys.version_info.minor == version[1]:
         return True
     else:
         return False
 
 def is_env_python(version):
+    """
+    A helper function to check exact match of Python version
+
+    Args:
+        version: int, str, tuple
+            Version number of Python to check. Can handle various formats to aid compatibility
+
+    Returns:
+        Boolean: true if version is identical, false if not
+
+    """
+
     if isinstance(version, str):
         version = int(version)
-        return is_env_python_minor(version)
+        return _is_env_python_minor(version)
     elif isinstance(version, int):
-        return is_env_python_minor(version)
+        return _is_env_python_minor(version)
     elif isinstance(version, tuple):
-        return is_env_python_major_and_minor(version)
+        return _is_env_python_major_and_minor(version)
     else:
         raise TypeError('Invalid data type: enter only str, int or tuple')
