ec25d8de9c2deb344eb514f7994bf16d096ef471
OscarvanVuren <vanvureno@cardiff.ac.uk>
2024-11-07 16:01:20 +0000

    Fixed test and included reference output

--- examples/utils_file_stitch.py
+++ examples/utils_file_stitch.py
@@ -12,15 +12,17 @@ def test_file_stitch():
     file_stitch(path='data/H+CH4_CH3+H2_path/H+CH4_CH3+H2.xyz',
                 out_fname='data/H+CH4_CH3+H2_path/H+CH4_CH3+H2_stitched.xyz',
                 data=mock_data,
-                lines_per_image=6)
+                lines_per_image=8)
     with open('data/H+CH4_CH3+H2_path/H+CH4_CH3+H2_stitched.xyz', 'r') as test:
         lines = test.readlines()
-        assert lines[0] == '6'
-        assert lines[1] == 'Frame 1'
-        assert lines[2] == '   C   0.63304144726145573 -0.1087864399671382 -0.56719035628535563   1'
-        assert lines[8] == '6'
-        assert lines[9] == 'Frame 1'
-        assert lines[10] == '   C   0.88562473811384212 0.97416578530588571 1.0453399509875991   1'
+    print(str(lines))
+
+    assert str(lines[0]) == '6\n'
+    assert str(lines[1]) == 'Frame 1\n'
+    assert str(lines[2]) == '   C   0.63304144726145573 -0.1087864399671382 -0.56719035628535563   1.0\n'
+    assert str(lines[8]) == '6\n'
+    assert str(lines[9]) == 'Frame 1\n'
+    assert str(lines[10]) == '   C   0.88562473811384212 0.97416578530588571 1.0453399509875991   1.0\n'
 
 
 test_file_stitch()
