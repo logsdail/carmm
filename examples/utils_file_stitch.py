1dde0f3c09ba62797958c125196aa51b5b6f5821
OscarvanVuren <vanvureno@cardiff.ac.uk>
2024-11-07 15:55:27 +0000

    Included test

---
+++ examples/utils_file_stitch.py
@@ -0,0 +1,32 @@
+
+import os
+
+
+def test_file_stitch():
+    import numpy as np
+
+    from carmm.utils.file_stitch import file_stitch
+
+    mock_data = np.ones(6)
+
+    file_stitch(path='data/H+CH4_CH3+H2_path/H+CH4_CH3+H2.xyz',
+                out_fname='data/H+CH4_CH3+H2_path/H+CH4_CH3+H2_stitched.xyz',
+                data=mock_data,
+                lines_per_image=6)
+    with open('data/H+CH4_CH3+H2_path/H+CH4_CH3+H2_stitched.xyz', 'r') as test:
+        lines = test.readlines()
+        assert lines[0] == '6'
+        assert lines[1] == 'Frame 1'
+        assert lines[2] == '   C   0.63304144726145573 -0.1087864399671382 -0.56719035628535563   1'
+        assert lines[8] == '6'
+        assert lines[9] == 'Frame 1'
+        assert lines[10] == '   C   0.88562473811384212 0.97416578530588571 1.0453399509875991   1'
+
+
+test_file_stitch()
+
+# Copy for reference
+os.system('cp data/H+CH4_CH3+H2_path/H+CH4_CH3+H2_stitched.xyz data/H+CH4_CH3+H2_path/H+CH4_CH3+H2_stitched_REF.xyz')
+
+# Remove the test file
+os.system('rm data/H+CH4_CH3+H2_path/H+CH4_CH3+H2_stitched.xyz')
