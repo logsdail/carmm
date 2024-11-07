2c2d26d6fa29fbd581b7eb55204e0c9dac0295e1
OscarvanVuren <vanvureno@cardiff.ac.uk>
2024-11-07 15:41:45 +0000

    Added docstring. Improved parsing for xyz trajectories

--- carmm/utils/file_stitch.py
+++ carmm/utils/file_stitch.py
@@ -1,15 +1,28 @@
 
-# Contains a function to stitch extra data onto the end of an exisiting .xyz file to create an extxyz file
+# Contains a function to stitch extra data onto the end of an existing .xyz file to create an extxyz file
 
-def file_stitch(path, out_fname, data):
+def file_stitch(path, out_fname, data, lines_per_image=None):
+
+    """
+
+    Args:
+        path: Path to xyz geometry file (str)
+        out_fname: Path to output file (str)
+        data: Array of data to be appended (array)
+        lines_per_image: Number of lines per image if appending to xyz trajectory (int)
+
+    """
 
     import shutil
     import fileinput
 
     shutil.copyfile(path, out_fname)
 
-    # File is n_atoms(1) + title(1) + n_coords(n) lines long
-    n_lines_image = data.size + 2
+    if lines_per_image is None:
+        # File is n_atoms(1) + title(1) + n_coords(n) lines long
+        n_lines_image = data.size + 2
+    else:
+        n_lines_image = lines_per_image
 
     image = 0
 
